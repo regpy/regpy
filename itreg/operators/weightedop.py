22a2743c2bf3f77e8cff7713e71cb93df9b544c8
Henrik Wiechers <henrik.wiechers@c170.math.uni-goettingen.de>
2016-09-01 15:24:13 +0200

    weightedop done

--- itreg/operators/weightedop.py
+++ itreg/operators/weightedop.py
@@ -4,15 +4,32 @@ __all__ = ['WeightedOp']
 
 
 class WeightedOp(LinearOperator):  
+    """The Weighted operator.
+    
+    Constructs an operator with weighted adjoint and derivative.
+    
+
+    Parameters
+    ----------
+    op : :class:`Operator <itreg.operators.Operator>`
+        The forward operator.
+    weight : array
+        The weight.
+    """
+    
+    
     def __init__(self, op, weight):
+        """Initialization of parameters"""
+        
         self.domx = op.domx
         self.domy = op.domy
         self.log = op.log
         self.op = op
         self.weight = weight
+
         
     def __call__(self, x):
         return self.weight * self.op(x)
 
     def adjoint(self, x):
-        return self.op.adjoint(self.weight * x)
+        return self.op.adjoint(self.weight*x)
