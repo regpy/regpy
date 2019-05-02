887028be1f9901ce5dc4d4dda63e7765aebb4e06
Christoph Ruegge <c.ruegge@math.uni-goettingen.de>
2019-05-02 19:02:43 +0200

    Add complex2real and real2complex helpers

--- itreg/util/__init__.py
+++ itreg/util/__init__.py
@@ -1,11 +1,6 @@
-"""Misc helper functions."""
-
-from .tests import test_adjoint
-from .cg_methods import CGNE_reg
-from .cg_methods import CG
-
 from functools import wraps
 from logging import getLogger
+import numpy as np
 
 
 @property
@@ -26,3 +21,23 @@ def memoized_property(prop):
             return getattr(self, attr)
 
     return mprop
+
+
+def complex2real(z, axis=-1):
+    assert z.dtype.kind == 'c'
+    x = np.lib.stride_tricks.as_strided(
+        z.real, shape=z.shape + (2,),
+        strides=z.strides + (z.real.dtype.itemsize,))
+    return np.moveaxis(x, -1, axis)
+
+
+def real2complex(x, axis=-1):
+    assert x.dtype.kind != 'c'
+    assert x.shape[axis] == 2
+    x = np.moveaxis(x, axis, -1)
+    if x.flags.c_contiguous:
+        return x.view(dtype=np.result_type(1j, x))[..., 0]
+    else:
+        z = np.array(x[..., 0], dtype=np.result_type(1j, x))
+        z.imag = x[..., 1]
+        return z
