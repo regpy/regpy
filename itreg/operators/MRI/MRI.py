1c05297ed586bf62a5bbdf613de694e9eac5250c
bjoern13 <mllerbjrn@ymail.com>
2019-05-03 09:11:13 +0200

    Updates MRI

--- itreg/operators/MRI/MRI.py
+++ itreg/operators/MRI/MRI.py
@@ -6,6 +6,7 @@ from itreg.grids import User_Defined
 
 import numpy as np
 import scipy as scp
+import matplotlib.pyplot as plt
 
 class parallel_MRI(NonlinearOperator):
     
@@ -29,10 +30,10 @@ class parallel_MRI(NonlinearOperator):
         
         if samplingIndx is None:
             undersampling = 2
-            nrcenterlines =  16
+            nrcenterlines =  8
                 #create sampling pattern P
             P = np.zeros((Nx,Ny))
-            P[:,undersampling:] = 1
+            P[:,np.arange(0, Ny, undersampling)] = 1
             P[:,int((Ny-nrcenterlines)/2):int((Ny+nrcenterlines)/2)] = 1
             samplingIndx = np.nonzero(np.reshape(P, (Nx*Ny, 1), order='F'))[0]
         
@@ -143,5 +144,83 @@ def myfft(rho):
 def myifft(data):
     Nx, Ny=data.shape
     rho=np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(data)))*scp.sqrt(Nx*Ny)
-    return rho      
+    return rho    
+
+class plots():
+    def __init__(self,
+                 op,
+                 reco,
+                 reco_data,
+                 data,
+                 exact_solution,
+                 Nx=None,
+                 Ny=None, 
+                 nr_coils=None,
+                 samplingIndx=None
+                 ):
+        
+        self.op=op
+        self.Nx=Nx or self.op.params.Nx
+        self.Ny=Ny or self.op.params.Ny
+        self.nr_coils=nr_coils or self.op.params.nr_coils
+        self.reco=reco
+        self.reco_data=reco_data
+        self.data=data
+        self.samplingIndx=samplingIndx or self.op.params.samplingIndx
+        self.exact_solution=exact_solution 
+        
+        
+        
+    def plot_samplingindex(self):
+        P=np.zeros((self.Nx*self.Ny))
+        P[self.samplingIndx]=1
+        P=P.reshape(self.Nx, self.Ny, order='F')
+        
+        fig = plt.figure(figsize=(8, 8))
+        vmax = abs(P).max()
+        X, Y=np.meshgrid(np.arange(0, 1, 1/self.Ny), np.arange(0, 1, 1/self.Nx))
+        cs=plt.pcolormesh(X, Y, P, cmap=plt.cm.seismic)
+        plt.title('sampling Index')
+        plt.colorbar()
+        plt.show()
+        
+    def plot_data(self, coil=None):
+        coil=coil or 0
+        P_data=np.zeros((self.Nx*self.Ny, self.nr_coils))
+        P_data2=np.zeros((self.Nx, self.Ny, self.nr_coils))
+        P_data[self.samplingIndx, coil]=self.reco_data[int(coil)*self.samplingIndx.shape[0]:int(coil+1)*self.samplingIndx.shape[0]]
+        for i in range(0, self.nr_coils):
+            P_data2[:, i]=P_data[:, i].reshape((self.Nx, self.Ny), order='F')
+        
+        data_print=np.zeros((self.Nx*self.Ny, self.nr_coils))
+        data_print2=np.zeros((self.Nx, self.Ny, self.nr_coils))
+        data_print[self.samplingIndx, coil]=self.data[int(coil)*self.samplingIndx.shape[0]:int(coil+1)*self.samplingIndx.shape[0]]
+        for i in range(0, self.nr_coils):
+            data_print2[:, i]=data_print[:, i].reshape((self.Nx, self.Ny), order='F')
+        
+        fig, axs = plt.subplots(1, 2,sharey=True,figsize=(8, 10))
+        X, Y=np.meshgrid(np.arange(0, 1, 1/self.Ny), np.arange(0, 1, 1/self.Nx))
+        cs=axs[0].contourf(X,Y,P_data2[:, 0],cmap=plt.cm.seismic)
+        axs[1].contourf(X,Y,data_print2[:, 0].reshape((self.Nx, self.Ny), order='F'),cmap=plt.cm.seismic)
+        fig.colorbar(cs)
+        axs[0].set_title('reco_data')
+        axs[1].set_title('data')
+        plt.show()
+        
+        
+    def plot_rho(self):
+        N=self.Nx*self.Ny
+        rho = np.reshape(self.reco[0:N],(self.Nx,self.Ny), order='F')
+                
+        fig, axs = plt.subplots(1, 2,sharey=True,figsize=(8, 10))
+        X, Y=np.meshgrid(np.arange(0, 1, 1/self.Ny), np.arange(0, 1, 1/self.Nx))
+        cs=axs[0].contourf(X,Y,rho,cmap=plt.cm.seismic)
+        axs[1].contourf(X,Y,self.exact_solution[0:N].reshape((self.Nx, self.Ny), order='F'),cmap=plt.cm.seismic)
+        fig.colorbar(cs)
+        axs[0].set_title('reco')
+        axs[1].set_title('exact solution')
+        plt.show()
+        
+
+            
                 
