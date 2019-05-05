592f6fc4492f1040c99e72e7c93aab98a088bae8
jakob.stoeriko <jakob.stoeriko@stud.uni-goettingen.de>
2019-05-05 13:01:42 +0200

    Added Linear Combinations and Composition

--- itreg/operators/__init__.py
+++ itreg/operators/__init__.py
@@ -59,6 +59,10 @@ class BaseOperator:
 
     def linearize(self, x):
         raise NotImplementedError
+    
+    #Jakob
+    def getArg(key):
+    	return self.__dict__.get(key)
 
 
 class NonlinearOperator(BaseOperator):
@@ -157,3 +161,72 @@ class Derivative(LinearOperator):
 
     def _adjoint(self, x):
         return self.params.op.get()._adjoint(x)
+
+################################################################################
+
+#Fragen:
+#1. Woher kommt das handle in NonlinearOperator und was macht das?
+#2. Was genau soll bei NonlinearOperator passieren wenn differentiate=True?
+#3. Meines Erachtens nach berechnet die _adjoint Funktion in der
+#	Derivative-Klasse aktuell die adjungierte des originalen Operators,
+#	müsste die nicht eigentlich die adjungierte der Ableitung berechnen?
+#4. Wie sieht im Allgemeinen die Adjungierte der Ableitung der Komposition
+#	zweier nicht-linearer Operatoren aus?
+#5. Derivative für lineare Operatoren nicht gebraucht? Was ist mit so Sachen wie
+#	k*x linearer Operator, aktuell würde linearize für die ableitung widerum
+#	k*x speichern, was dann zu falschen Ergebnissen führt.
+#6.	NonlinearOperator hat keine property adjoint, soll das so?
+#7. Kann derivative auch ne property werden? Dann wäre das ganze einheitlich
+
+
+#TODO Testing, need to define some simple cases first
+class LinearCombination(LinearOperator):
+	#TODO Domain and range query for scalars missing
+	def __init__(self, f, g, scalar_f, scalar_g):
+		if(f.domain() != g.domain() or f.range() != g.range()):
+			raise ValueError('Domains and Ranges of Operators must be the same')
+		args = {f:f, g:g, scalar_f:scalar_f, scalar_g:scalar_g}
+		super().__init__(Params(f.domain(), f.range(), args))
+	
+	def _eval(self, x):
+		return (self.getArg(scalar_f) * self.getArg(f)._eval(x) +
+				self.getArg(scalar_g) * self.getArg(g)._eval(x))
+	
+	#f._adjoint(x) oder f.adjoint._eval(x) verwenden?
+	def _adjoint(self, x):
+		return self.getArg(scalar_f) * self.getArg(f).adjoint._eval(x) + self.getArg(scalar_g) * self.getArg(g).adjoint._eval(x)
+
+class Composition(NonlinearOperator):
+	def __init__(self,f,g):
+		if(f.domain() != g.range()):
+			raise ValueError('For f∘g, domain of f and' +
+							 'range of g must be the same')
+		super().__init__(Params(g.domain(), f.range(), {f:f, g:g}))
+	
+	def _eval(self, x, differentiate = False):
+		return self.getArg(f)._eval(self.getArg(g)._eval(x))
+	
+	def _adjoint(self, x):
+		return self.getArg(g)._adjoint(self.getArg(f)._adjoint(x))
+	
+	#not sure if this works
+	def _derivative(self, x):
+		g, dg = self.getArg(g).linearize(x)
+		fg, dfg = self.getArg(f).linearize(g)
+		return dg*dfg
+		
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
