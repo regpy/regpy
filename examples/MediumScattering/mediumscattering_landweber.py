d0b1aed1c3e777505c2999f1ea12ea1f5351e6f0
Christoph Ruegge <c.ruegge@math.uni-goettingen.de>
2019-05-15 19:10:34 +0200

    Update MediumScattering example

--- examples/MediumScattering/mediumscattering_landweber.py
+++ examples/MediumScattering/mediumscattering_landweber.py
@@ -3,20 +3,20 @@ import setpath
 import itreg
 
 from itreg.operators import MediumScattering, CoordinateProjection
-# TODO from itreg.spaces import Sobolev
-from itreg.spaces import L2, HilbertPullBack
-from itreg.solvers import Landweber
+from itreg.spaces import L2, H1, HilbertPullBack
+from itreg.solvers import Landweber, HilbertSpaceSetting
 # TODO from itreg.util import test_adjoint
 import itreg.stoprules as rules
 import itreg.util as util
 
+from functools import partial
 import numpy as np
 import logging
 
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(name)-40s :: %(message)s')
+    format='%(asctime)s %(levelname)s %(name)-20s :: %(message)s')
 
 scattering = MediumScattering(
     gridshape=(65, 65),
@@ -24,8 +24,12 @@ scattering = MediumScattering(
     wave_number=1,
     inc_directions=util.linspace_circle(16),
     meas_directions=util.linspace_circle(16),
-    support=lambda grid, radius: np.max(np.abs(grid.coords), axis=0) <= radius,
+    # support=lambda grid, radius: np.max(np.abs(grid.coords), axis=0) <= radius,
     amplitude=False)
+
+contrast = scattering.domain.zeros()
+r = np.linalg.norm(scattering.domain.coords, axis=0)
+contrast[r < 1] = np.exp(-1/(1 - r[r < 1]**2))
 
 projection = CoordinateProjection(
     scattering.domain,
@@ -34,19 +38,26 @@ embedding = projection.adjoint
 
 op = scattering * embedding
 
-exact_solution = op.domain.ones()
+exact_solution = projection(contrast)
 exact_data = op(exact_solution)
-noise = 0.03 * op.codomain.rand(np.random.randn)
+noise = 0.03 * op.codomain.randn()
 data = exact_data + noise
 noiselevel = np.linalg.norm(noise)
 init = 1.1 * op.domain.ones()
 
-domain = HilbertPullBack(L2, embedding, inverse='cholesky')
-codomain = L2
+setting = HilbertSpaceSetting(
+    op=op,
+    domain=HilbertPullBack(partial(H1, index=1), embedding, inverse='cholesky'),
+    codomain=L2)
 
-landweber = Landweber(op, domain, codomain, data, init, stepsize=0.01)
+landweber = Landweber(setting, data, init, stepsize=0.01)
 stoprule = (
     rules.CountIterations(100) +
-    rules.Discrepancy(codomain.norm, data, noiselevel=0.1, tau=1))
+    rules.Discrepancy(setting.codomain.norm, data, noiselevel=0.1, tau=1))
 
 reco, reco_data = landweber.run(stoprule)
+solution = embedding(reco)
+
+import matplotlib.pylab as plt
+plt.imshow(np.abs(solution))
+plt.show()
