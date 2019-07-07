25efb0d73802647f5af2b2aec70bfb1fd892fca1
hendrik_mueller <hendrikmuellerasdf@gmail.com>
2019-07-07 15:23:03 +0200

    Separate finite element space of domain and codomain for reaction coefficient problem

--- examples/Reaction/reactioncoefficient_landweber_1d.py
+++ examples/Reaction/reactioncoefficient_landweber_1d.py
@@ -17,32 +17,39 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s %(name)-40s :: %(message)s')
 
 
-meshsize=100
+meshsize_domain=100
+meshsize_codomain=100
 
 from ngsolve import *
-mesh = Make1DMesh(meshsize)
-fes = H1(mesh, order=2, dirichlet="left|right")
-disc= NGSolveDiscretization(fes)
+
+mesh = Make1DMesh(meshsize_domain)
+fes_domain = L2(mesh, order=2, dirichlet="left|right")
+domain = NGSolveDiscretization(fes_domain)
+
+mesh = Make1DMesh(meshsize_codomain)
+fes_codomain = H1(mesh, order=2, dirichlet="left|right")
+codomain = NGSolveDiscretization(fes_codomain)
 
 rhs=10*x**2
-op = Coefficient(disc, rhs=rhs, bc_left=1, bc_right=1.1, diffusion=False, reaction=True)
+op = Coefficient(domain, codomain=codomain, rhs=rhs, bc_left=1, bc_right=1.1, diffusion=False, reaction=True)
 
 #exact_solution = np.linspace(1, 2, 201)
 exact_solution_coeff = 1+x
-gfu_exact_solution=GridFunction(op.fes)
+gfu_exact_solution=GridFunction(op.fes_domain)
 gfu_exact_solution.Set(exact_solution_coeff)
 exact_solution=gfu_exact_solution.vec.FV().NumPy()
 exact_data = op(exact_solution)
-data=exact_data
+noise = 0.01 * op.codomain.randn()
+data=exact_data+noise
 
-gfu=GridFunction(op.fes)
+gfu=GridFunction(op.fes_codomain)
 for i in range(201):
     gfu.vec[i]=data[i]
     
 Symfunc=CoefficientFunction(gfu)
 func=np.zeros(201)
 for i in range(0, 201):
-    mip=op.fes.mesh(i/200)
+    mip=op.fes_codomain.mesh(i/200)
     func[i]=Symfunc(mip)
     
 plt.plot(func)
@@ -59,13 +66,14 @@ adj=deriv.adjoint(np.linspace(1, 2, 201))
 
 #init=np.concatenate((np.linspace(1, 2, 101), np.ones(100)))
 init=1+x**3
-init_gfu=GridFunction(op.fes)
+init_gfu=GridFunction(op.fes_domain)
 init_gfu.Set(init)
 init_solution=init_gfu.vec.FV().NumPy().copy()
+init_plot=init_solution.copy()
 init_data=op(init_solution)
 
-from itreg.spaces import L2
-setting = HilbertSpaceSetting(op=op, domain=L2, codomain=L2)
+from itreg.spaces import L2, NGSolveSpace
+setting = HilbertSpaceSetting(op=op, domain=NGSolveSpace, codomain=NGSolveSpace)
 
 landweber = Landweber(setting, data, init_solution, stepsize=1)
 #irgnm_cg = IRGNM_CG(op, data, init, cgmaxit = 50, alpha0 = 1, alpha_step = 0.9, cgtol = [0.3, 0.3, 1e-6])
@@ -81,22 +89,24 @@ plt.plot(exact_solution, label='exact')
 plt.legend()
 plt.show()
 
-gfu=GridFunction(op.fes)
-gfu2=GridFunction(op.fes)
-gfu3=GridFunction(op.fes)
-for i in range(201):
+N=op.fes_domain.ndof
+
+gfu=GridFunction(op.fes_domain)
+gfu2=GridFunction(op.fes_domain)
+gfu3=GridFunction(op.fes_domain)
+for i in range(N):
     gfu.vec[i]=reco[i]
     gfu2.vec[i]=exact_solution[i]
-    gfu3.vec[i]=init_solution[i]
+    gfu3.vec[i]=init_plot[i]
     
 Symfunc=CoefficientFunction(gfu)
 Symfunc2=CoefficientFunction(gfu2)
 Symfunc3=CoefficientFunction(gfu3)
-func=np.zeros(201)
-func2=np.zeros(201)
-func3=np.zeros(201)
-for i in range(0, 201):
-    mip=op.fes.mesh(i/200)
+func=np.zeros(N)
+func2=np.zeros(N)
+func3=np.zeros(N)
+for i in range(0, N):
+    mip=op.fes_domain.mesh(i/N)
     func[i]=Symfunc(mip)
     func2[i]=Symfunc2(mip)
     func3[i]=Symfunc3(mip)
@@ -112,29 +122,35 @@ plt.show()
 
 
 
-gfu=GridFunction(op.fes)
-gfu2=GridFunction(op.fes)
-gfu3=GridFunction(op.fes)
+gfu=GridFunction(op.fes_codomain)
+gfu2=GridFunction(op.fes_codomain)
+gfu3=GridFunction(op.fes_codomain)
+gfu4=GridFunction(op.fes_codomain)
 for i in range(201):
     gfu.vec[i]=reco_data[i]
     gfu2.vec[i]=exact_data[i]
     gfu3.vec[i]=init_data[i]
+    gfu4.vec[i]=data[i]
     
 Symfunc=CoefficientFunction(gfu)
 Symfunc2=CoefficientFunction(gfu2)
 Symfunc3=CoefficientFunction(gfu3)
+Symfunc4=CoefficientFunction(gfu4)
 func=np.zeros(201)
 func2=np.zeros(201)
 func3=np.zeros(201)
+func4=np.zeros(201)
 for i in range(0, 201):
-    mip=op.fes.mesh(i/200)
+    mip=op.fes_codomain.mesh(i/200)
     func[i]=Symfunc(mip)
     func2[i]=Symfunc2(mip)
     func3[i]=Symfunc3(mip)
+    func4[i]=Symfunc4(mip)
     
 plt.plot(func, label='reco')
 plt.plot(func2, label='exact')
 plt.plot(func3, label='init')
+plt.plot(func4, label='data')
 plt.legend()
 plt.show()
 
--- itreg/operators/NGSolveProblems/Coefficient.py
+++ itreg/operators/NGSolveProblems/Coefficient.py
@@ -5,7 +5,7 @@
 from itreg.operators import NonlinearOperator
 
 import numpy as np
-import netgen.gui
+#import netgen.gui
 from ngsolve import *
 from netgen.geom2d import unit_square
 from ngsolve.meshes import Make1DMesh, MakeQuadMesh
@@ -32,10 +32,10 @@ class Coefficient(NonlinearOperator):
 
         
         #Define mesh and finite element space
-        self.fes=domain.fes
+        self.fes_domain=domain.fes
         #self.mesh=self.fes.mesh
         
-        self.fes=codomain.fes
+        self.fes_codomain=codomain.fes
 #        if dim==1:
 #            self.mesh = Make1DMesh(meshsize)
 #            self.fes = H1(self.mesh, order=2, dirichlet="left|right")
@@ -44,35 +44,35 @@ class Coefficient(NonlinearOperator):
 #            self.fes = H1(self.mesh, order=2, dirichlet="left|top|right|bottom")
 
         #grid functions for later use 
-        self.gfu = GridFunction(self.fes)  # solution, return value of _eval
-        self.gfu_bdr=GridFunction(self.fes) #grid function holding boundary values
+        self.gfu = GridFunction(self.fes_codomain)  # solution, return value of _eval
+        self.gfu_bdr=GridFunction(self.fes_codomain) #grid function holding boundary values
         
-        self.gfu_integrator = GridFunction(self.fes) #grid function for defining integrator (bilinearform)
-        self.gfu_integrator_codomain = GridFunction(self.fes)
-        self.gfu_rhs = GridFunction(self.fes) #grid function for defining right hand side (linearform)
+        self.gfu_integrator = GridFunction(self.fes_domain) #grid function for defining integrator (bilinearform)
+        self.gfu_integrator_codomain = GridFunction(self.fes_codomain)
+        self.gfu_rhs = GridFunction(self.fes_codomain) #grid function for defining right hand side (Linearform)
         
-        self.gfu_inner=GridFunction(self.fes) #grid function for inner computation in derivative and adjoint
-        self.gfu_toret=GridFunction(self.fes) #grid function for returning values in adjoint and derivative
+        self.gfu_inner=GridFunction(self.fes_codomain) #grid function for inner computation in derivative and adjoint
+        self.gfu_toret=GridFunction(self.fes_domain) #grid function for returning values in adjoint and derivative
        
-        u = self.fes.TrialFunction()  # symbolic object
-        v = self.fes.TestFunction()   # symbolic object 
+        u = self.fes_codomain.TrialFunction()  # symbolic object
+        v = self.fes_codomain.TestFunction()   # symbolic object 
 
         #Define Bilinearform, will be assembled later        
-        self.a = BilinearForm(self.fes, symmetric=True)
+        self.a = BilinearForm(self.fes_codomain, symmetric=True)
         if self.diffusion:
-            self.a += SymbolicBFI(grad(u)*grad(v)*self.gfu_integrator)
+            self.a += SymbolicBFI(grad(u)*grad(v)*self.gfu_integrator_codomain)
         elif self.reaction:
-            self.a += SymbolicBFI(grad(u)*grad(v)+u*v*self.gfu_integrator)
+            self.a += SymbolicBFI(grad(u)*grad(v)+u*v*self.gfu_integrator_codomain)
 
         #Define Linearform, will be assembled later        
-        self.f=LinearForm(self.fes)
+        self.f=LinearForm(self.fes_codomain)
         self.f += SymbolicLFI(self.gfu_rhs*v)
         
         #Precompute Boundary values and boundary valued corrected rhs
         if self.dim==1:
-            self.gfu_bdr.Set([bc_left, bc_right], definedon=self.fes.mesh.Boundaries("left|right"))
+            self.gfu_bdr.Set([bc_left, bc_right], definedon=self.fes_codomain.mesh.Boundaries("left|right"))
         elif self.dim==2:
-            self.gfu_bdr.Set([bc_left, bc_top, bc_right, bc_bottom], definedon=self.fes.mesh.Boundaries("left|top|right|bottom"))
+            self.gfu_bdr.Set([bc_left, bc_top, bc_right, bc_bottom], definedon=self.fes_codomain.mesh.Boundaries("left|top|right|bottom"))
         self.r=self.f.vec.CreateVector()
         
         super().__init__(domain, codomain)
@@ -80,6 +80,7 @@ class Coefficient(NonlinearOperator):
     def _eval(self, diff, differentiate, **kwargs):
         #Assemble Bilinearform
         self.gfu_integrator.vec.FV().NumPy()[:]=diff 
+        self.gfu_integrator_codomain.Set(self.gfu_integrator)
 #       self.gfu_integrator.Set(diff)
         self.a.Assemble()
             
@@ -137,6 +138,6 @@ class Coefficient(NonlinearOperator):
         return self.gfu_toret.vec.FV().NumPy().copy()
         
     def _Solve(self, bilinear, rhs, boundary=False):
-        return bilinear.mat.Inverse(freedofs=self.fes.FreeDofs()) * rhs
+        return bilinear.mat.Inverse(freedofs=self.fes_codomain.FreeDofs()) * rhs
 
-    
+
