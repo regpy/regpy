6f462b135b0661130efa08ed35efcdf0b2cb673d
Christoph Ruegge <c.ruegge@math.uni-goettingen.de>
2019-09-13 17:55:43 +0200

    spaces.ngsolve: code style

--- itreg/spaces/ngsolve.py
+++ itreg/spaces/ngsolve.py
@@ -1,82 +1,56 @@
-import numpy as np
 import ngsolve as ngs
+import numpy as np
 
 from . import Grid
-from .hilbert import HilbertSpace, L2, Sobolev, SobolevBoundary, L2Boundary
+from .hilbert import HilbertSpace, L2, L2Boundary, Sobolev, SobolevBoundary
 
 
 class NGSolveDiscretization(Grid):
     def __init__(self, fes, *args, **kwargs):
-        self.fes=fes
-#        gfu=ngs.GridFunction(self.fes)
-#        self.u=gfu.vec.CreateVector()
-#        self.v=gfu.vec.CreateVector()
-#        self.toret=np.empty(fes.ndof)
-
-        #u, v=self.fes.TnT()
-        self.a=ngs.BilinearForm(self.fes, symmetric=True)
-        #self.a+=SymbolicBFI(u*v)
-        #self.a.Assemble()
-
-        #self.b=self.a.mat.Inverse(freedofs=self.fes.FreeDofs())
-        #self.b=self.a.mat
-
-        self.gfu_in=ngs.GridFunction(self.fes)
-        self.gfu_toret=ngs.GridFunction(self.fes)
+        self.fes = fes
+        self.a = ngs.BilinearForm(self.fes, symmetric=True)
+        self.gfu_in = ngs.GridFunction(self.fes)
+        self.gfu_toret = ngs.GridFunction(self.fes)
         super().__init__(np.empty(fes.ndof), *args, **kwargs)
 
-#    def inner(self, x):
-#        self.v.FV().NumPy()[:]=x
-#        toret=np.zeros(self.fes.ndof)
-#        for i in range(self.fes.ndof):
-#            self.u.FV().NumPy()[:]=np.eye(1, self.fes.ndof, i)[0]
-#            toret[i]=InnerProduct(self.u, self.v)
-#        return toret
-
     def apply_gram(self, x):
-        self.gfu_in.vec.FV().NumPy()[:]=x
-        self.gfu_toret.vec.data = self.a.mat*self.gfu_in.vec
+        self.gfu_in.vec.FV().NumPy()[:] = x
+        self.gfu_toret.vec.data = self.a.mat * self.gfu_in.vec
         return self.gfu_toret.vec.FV().NumPy().copy()
 
     def apply_gram_inverse(self, x):
-        self.gfu_in.vec.FV().NumPy()[:]=x
-        self.gfu_toret.vec.data = self.b*self.gfu_in.vec
+        self.gfu_in.vec.FV().NumPy()[:] = x
+        self.gfu_toret.vec.data = self.b * self.gfu_in.vec
         return self.gfu_toret.vec.FV().NumPy().copy()
 
 
 class NGSolveBoundaryDiscretization(Grid):
     def __init__(self, fes, fes_bdr, ind, *args, **kwargs):
-        self.fes=fes
-
-        #u, v=self.fes.TnT()
-        self.a=ngs.BilinearForm(self.fes, symmetric=True)
-
-        self.gfu_in=ngs.GridFunction(self.fes)
-        self.gfu_toret=ngs.GridFunction(self.fes)
-
+        self.fes = fes
+        self.a = ngs.BilinearForm(self.fes, symmetric=True)
+        self.gfu_in = ngs.GridFunction(self.fes)
+        self.gfu_toret = ngs.GridFunction(self.fes)
         super().__init__(np.empty(fes.ndof), *args, **kwargs)
 
     def apply_gram(self, x):
-        self.gfu_in.vec.FV().NumPy()[:]=x
-        self.gfu_toret.vec.data = self.a.mat*self.gfu_in.vec
+        self.gfu_in.vec.FV().NumPy()[:] = x
+        self.gfu_toret.vec.data = self.a.mat * self.gfu_in.vec
         return self.gfu_toret.vec.FV().NumPy().copy()
 
     def apply_gram_inverse(self, x):
-        self.gfu_in.vec.FV().NumPy()[:]=x
-        self.gfu_toret.vec.data = self.b*self.gfu_in.vec
+        self.gfu_in.vec.FV().NumPy()[:] = x
+        self.gfu_toret.vec.data = self.b * self.gfu_in.vec
         return self.gfu_toret.vec.FV().NumPy().copy()
 
 
 @L2.register(NGSolveDiscretization)
-class NGSolveFESSpace_L2(HilbertSpace):
+class L2NGSolve(HilbertSpace):
     def __init__(self, discr):
-        self.discr = discr
-
-        u, v=self.discr.fes.TnT()
-        self.discr.a+=ngs.SymbolicBFI(u*v)
+        super().__init__(discr)
+        u, v = self.discr.fes.TnT()
+        self.discr.a += ngs.SymbolicBFI(u * v)
         self.discr.a.Assemble()
-
-        self.discr.b=self.discr.a.mat.Inverse(freedofs=self.discr.fes.FreeDofs())
+        self.discr.b = self.discr.a.mat.Inverse(freedofs=self.discr.fes.FreeDofs())
 
     @property
     def gram(self):
@@ -88,16 +62,13 @@ class NGSolveFESSpace_L2(HilbertSpace):
 
 
 @Sobolev.register(NGSolveDiscretization)
-class NGSolveFESSpace_H1(HilbertSpace):
+class SobolevNGSolve(HilbertSpace):
     def __init__(self, discr):
-        self.discr = discr
-
-        u, v=self.discr.fes.TnT()
-        self.discr.a+=ngs.SymbolicBFI(u*v+ngs.grad(u)*ngs.grad(v))
+        super().__init__(discr)
+        u, v = self.discr.fes.TnT()
+        self.discr.a += ngs.SymbolicBFI(u * v + ngs.grad(u) * ngs.grad(v))
         self.discr.a.Assemble()
-
-        self.discr.b=self.discr.a.mat.Inverse(freedofs=self.discr.fes.FreeDofs())
-
+        self.discr.b = self.discr.a.mat.Inverse(freedofs=self.discr.fes.FreeDofs())
 
     @property
     def gram(self):
@@ -109,15 +80,16 @@ class NGSolveFESSpace_H1(HilbertSpace):
 
 
 @SobolevBoundary.register(NGSolveDiscretization)
-class NGSolveFESSpace_H1_bdr(HilbertSpace):
+class SobolevBoundaryNGSolve(HilbertSpace):
     def __init__(self, discr):
-        self.discr = discr
-
-        u, v=self.discr.fes.TnT()
-        self.discr.a+=ngs.SymbolicBFI(u.Trace()*v.Trace()+u.Trace().Deriv()*v.Trace().Deriv(), definedon=self.discr.fes.mesh.Boundaries("cyc"))
+        super().__init__(discr)
+        u, v = self.discr.fes.TnT()
+        self.discr.a += ngs.SymbolicBFI(
+            u.Trace() * v.Trace() + u.Trace().Deriv() * v.Trace().Deriv(),
+            definedon=self.discr.fes.mesh.Boundaries("cyc")
+        )
         self.discr.a.Assemble()
-
-        self.discr.b=self.discr.a.mat.Inverse(freedofs=self.discr.fes.FreeDofs())
+        self.discr.b = self.discr.a.mat.Inverse(freedofs=self.discr.fes.FreeDofs())
 
     @property
     def gram(self):
@@ -129,15 +101,13 @@ class NGSolveFESSpace_H1_bdr(HilbertSpace):
 
 
 @L2Boundary.register(NGSolveDiscretization)
-class NGSolveFESSpace_L2_bdr(HilbertSpace):
+class L2BoundaryNGSolve(HilbertSpace):
     def __init__(self, discr):
-        self.discr = discr
-
-        u, v=self.discr.fes.TnT()
-        self.discr.a+=ngs.SymbolicBFI(u.Trace()*v.Trace(), definedon=self.discr.fes.mesh.Boundaries("cyc"))
+        super().__init__(discr)
+        u, v = self.discr.fes.TnT()
+        self.discr.a += ngs.SymbolicBFI(u.Trace() * v.Trace(), definedon=self.discr.fes.mesh.Boundaries("cyc"))
         self.discr.a.Assemble()
-
-        self.discr.b=self.discr.a.mat.Inverse(freedofs=self.discr.fes.FreeDofs())
+        self.discr.b = self.discr.a.mat.Inverse(freedofs=self.discr.fes.FreeDofs())
 
     @property
     def gram(self):
