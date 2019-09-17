aa797fc73545fa87fac40e1d7934c5b7468a6445
Christoph Ruegge <c.ruegge@math.uni-goettingen.de>
2019-09-17 16:42:34 +0200

    Add dependencies to setup.py and singularity.def

--- setup.py
+++ setup.py
@@ -2,5 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='itreg',
-    packages=setuptools.find_packages()
+    version='0.1',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'matplotlib>=2.2,<3.0',
+        'numpy>=1.14,<2.0',
+        'scipy>=1.1,<2.0',
+        'pyNFFT>=1.3,<2.0',
+    ],
+    python_requires='>=3.6,<4.0',
 )
