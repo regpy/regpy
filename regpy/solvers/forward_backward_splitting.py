18d766d75dac983aea8855bfb571eece1ce85a6e
hendrik_mueller <hendrikmuellerasdf@gmail.com>
2020-05-03 16:41:43 +0200

    Forward-Backward Splitting + Example

---
+++ regpy/solvers/forward_backward_splitting.py
@@ -0,0 +1,36 @@
+import logging
+import numpy as np
+
+from regpy.solvers import Solver
+from regpy import util
+
+"""
+Minimizes data_fidelity(f)+regpar*penalty(f) with forward backward splitting
+data_fidelity and penalty are functionals
+"""
+
+class Forward_Backward_Splitting(Solver):
+    def __init__(self, setting, data_fidelity, penalty, init, tau = 1, regpar = 1):
+        
+        super().__init__()
+        self.setting = setting
+        """The problem setting."""
+        self.regpar = regpar
+        """The regularization parameter."""
+        self.tau = tau
+        """The proximal operator parameter"""
+
+        self.data_fidelity = data_fidelity
+        self.penalty = penalty
+        """The functional of the data fidelity term and the penalty term"""
+        
+        self.x = init
+        self.y = self.setting.op(self.x)
+        
+    def _next(self):
+        self.x-=self.tau*self.data_fidelity._gradient(self.x)
+        self.x = self.penalty._proximal(self.x, self.regpar*self.tau)
+        """Note: If F = alpha G, then prox_{tau, F} = prox_{alpha * tau, G}"""
+        
+        self.y = self.setting.op(self.x)
+        
