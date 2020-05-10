0116cf2a4e97b5af44ac2df7ffb3e12381bcf99e
hendrik_mueller <hendrikmuellerasdf@gmail.com>
2020-05-10 19:18:50 +0200

    Insert cgpars in ADMM

--- regpy/solvers/admm.py
+++ regpy/solvers/admm.py
@@ -9,7 +9,7 @@ from regpy.solvers.tikhonov import TikhonovCG
 """The ADMM algorithm"""
 
 class ADMM(Solver):
-    def __init__(self,  setting, data_fidelity, penalty, init_v1, init_v2, init_p1, init_p2, gamma = 1, regpar = 1, proximal_pars_data_fidelity = None, proximal_pars_penalty = None):
+    def __init__(self,  setting, data_fidelity, penalty, init_v1, init_v2, init_p1, init_p2, gamma = 1, regpar = 1, proximal_pars_data_fidelity = None, proximal_pars_penalty = None, cgpars = None):
         super().__init__()
         self.setting = setting
         assert self.setting.op.linear
@@ -26,11 +26,17 @@ class ADMM(Solver):
         self.proximal_pars_data_fidelity = proximal_pars_data_fidelity
         self.proximal_pars_penalty = proximal_pars_penalty
 
+        if cgpars is None:
+            cgpars = {}
+        self.cgpars = cgpars
+        """The additional `regpy.solvers.tikhonov.TikhonovCG` parameters."""
+
         self.x, self.y = TikhonovCG(
             setting=HilbertSpaceSetting(self.setting.op, self.setting.Hdomain, self.setting.Hcodomain),
             data=self.v1+self.p1,
             xref=self.v2+self.p2
-            regpar=1
+            regpar=1,
+            **self.cgpars
         ).run()
 
     def _next(self):
@@ -43,5 +49,6 @@ class ADMM(Solver):
             setting=HilbertSpaceSetting(self.setting.op, self.setting.Hdomain, self.setting.Hcodomain),
             data=self.v1+self.p1,
             xref=self.v2+self.p2
-            regpar=1
+            regpar=1,
+            **self.cgpars
         ).run()
