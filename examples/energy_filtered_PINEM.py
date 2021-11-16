e8a1651f6059f526204af057086c94495948a5c8
John Gaida <johnhenri.gaida@stud.uni-goettingen.de>
2021-11-16 18:05:43 +0100

    Slightly modified example * normalize grid extends from 0 to 1 such that the Fresnel number * smaller defocus (larger Fresnel number) * plot amplitude and phase instead of real and imag part

--- examples/energy_filtered_PINEM.py
+++ examples/energy_filtered_PINEM.py
@@ -1,7 +1,7 @@
 from regpy.solvers.irgnm import IrgnmCG
 
 from regpy.operators.PINEM import wave_field_reco_PINEM
-from regpy.hilbert import L2, DirectSum
+from regpy.hilbert import L2
 from regpy.discrs import UniformGrid
 from regpy.solvers import HilbertSpaceSetting
 import regpy.stoprules as rules
@@ -17,13 +17,13 @@ logging.basicConfig(
 )
 
 # Example parameters
-fresnelNumber = 1e-3    # Fresnel-number of the simulated imaging system, associated with the unit-lengthscale
+fresnelNumber = 5e3    # Fresnel-number of the simulated imaging system, associated with the unit-lengthscale
                         # in grid (i.e. with the size of one pixel for the above choice of grid)
 noise_level = 0.01      # Noise level in the simulated data
 
 # Uniform grid
 Xdim= 256; Ydim= 256
-grid = UniformGrid(np.arange(Xdim), np.arange(Ydim)).complex_space()
+grid = UniformGrid(np.linspace(0,1,Xdim,endpoint=False), np.linspace(0,1,Ydim,endpoint=False)).complex_space()
 [Xco,Yco] = np.meshgrid(np.arange(-1,1,2/Xdim),np.arange(-1,1,2/Ydim))
 mask = (abs(Xco+0.2)<=0.2) & (abs(Yco)<=0.4)
 mask = mask | (abs((Xco-0.35)*(Xco-0.35)+(Yco-0.35)*(Yco-0.35))<=0.01)
@@ -33,12 +33,12 @@ op = wave_field_reco_PINEM(grid, fresnelNumber,mask.astype(complex))
 
 # Create phantom phase-image (= padded example-image)
 picture = ascent()
-exact_solution = picture[-Xdim//2:,-Ydim//2:].astype(np.float64) \
-    + 1j*picture[:Xdim//2,:Ydim//2].astype(np.float64)
+exact_solution = picture[-Xdim//2:,-Ydim//2:].astype(np.float64)/255 \
+    * np.exp(1j*2*np.pi*picture[:Xdim//2,:Ydim//2].astype(np.float64)/255)
 exact_solution /= abs(exact_solution).max()
 pad_amount = tuple([(grid.shape[0] - exact_solution.shape[0])//2, (grid.shape[1] - exact_solution.shape[1])//2])
 exact_solution = np.pad(exact_solution, pad_amount, 'constant', constant_values=0)
-exact_solution = exact_solution.astype(complex);
+exact_solution = exact_solution.astype(complex)
 
 # Create exact and noisy data
 exact_data = op(exact_solution)
@@ -65,22 +65,28 @@ reco,reco_data = solver.run(stoprule)
 #reco = mask
 
 # Plot reults
-fig, axs = plt.subplots(2, 2)
-axs[0,0].set_title('Exact solution (real part)')
-axs[0,0].imshow(mask*exact_solution.real)
-axs[0,1].set_title('Exact solution (imag. part)')
-axs[0,1].imshow(mask*exact_solution.imag)
-axs[1,0].set_title('Reconstruction (real part)')
-axs[1,0].imshow(reco.real)
-axs[1,1].set_title('Reconstruction (imag. part)')
-axs[1,1].imshow(reco.imag)
+fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+axs[0,0].set_title('Exact solution (abs)')
+im = axs[0,0].imshow(mask*np.abs(exact_solution))
+fig.colorbar(im,ax=axs[0,0])
+axs[0,1].set_title('Exact solution (phase)')
+im = axs[0,1].imshow(mask*np.angle(exact_solution))
+fig.colorbar(im,ax=axs[0,1])
+axs[1,0].set_title('Reconstruction (abs)')
+im = axs[1,0].imshow(mask*np.abs(reco))
+fig.colorbar(im,ax=axs[1,0])
+axs[1,1].set_title('Reconstruction (phase)')
+im = axs[1,1].imshow(mask*np.angle(reco))
+fig.colorbar(im,ax=axs[1,1])
 
 data_comp = op.codomain.split(data)
 reco_data_comp = op.codomain.split(reco_data)
-fig2, axs2 = plt.subplots(2, 2)
-for j in range(2):
-    axs2[0,j].imshow(data_comp[j])
+fig2, axs2 = plt.subplots(2, len(data_comp), sharex=True, sharey=True)
+for j in range(len(data_comp)):
+    im = axs2[0,j].imshow(data_comp[j])
+    fig2.colorbar(im,ax=axs2[0,j])
     axs2[0,j].set_title('Simulated data')
-    axs2[1,j].imshow(reco_data_comp[j])
+    im = axs2[1,j].imshow(reco_data_comp[j])
+    fig2.colorbar(im,ax=axs2[1,j])
     axs2[1,j].set_title('reconstructed data')
 plt.show()
