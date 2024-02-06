5391662c5e47ce9d2cd63a83c7ad203e2cc851b6
ehlers26 <b.ehlers@math.uni-goettingen.de>
2024-02-06 14:49:19 +0100

    added test for space registration

---
+++ tests/new_tests.py
@@ -0,0 +1,44 @@
+from regpy.discrs import *
+from regpy import hilbert
+
+
+# Tests if the Spaces registry works
+
+def test_L2_uniform_grid():
+    grid = UniformGrid(10,11)
+    hilbert.L2(grid)
+
+def test_Sobolev_uniform_grid():
+    grid = UniformGrid(10,11)
+    hilbert.Sobolev(grid)
+
+def test_L2_grid():
+    grid = Grid(axisdata = (np.linspace(0,10,5),np.logspace(-1,5,15)))
+    hilbert.L2(grid)
+
+def test_L2_directsum():
+    grid1 = Grid(axisdata = (np.linspace(0,10,5),np.logspace(-1,5,15)))
+    grid2 = UniformGrid(10,11)
+    grid = grid1 + grid2
+    hilbert.L2(grid)
+
+def test_Sobolev_directsum():
+    grid = UniformGrid(10,11)
+    grid = grid + grid
+    hilbert.Sobolev(grid)
+
+
+
+# Does not Work 
+
+# def test_L2Boundary_directsum():
+#     grid1 = Grid(axisdata = (np.linspace(0,10,5),np.logspace(-1,5,15)))
+#     grid = UniformGrid(10,11)
+#     grid = grid + grid
+#     hilbert.L2Boundary(grid)
+
+# def test_Hm_grid():
+#     # grid = Grid(axisdata = (np.linspace(0,10,5),np.logspace(-1,5,15)))
+#     grid = Grid(10,11)
+#     hilbert.Hm(grid,)
+    
