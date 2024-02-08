fb4f2204d0188879c2b97fe31a37aa81b770ea0e
Jakob Florian Oberender <j.oberender@math.uni-goettingen.de>
2024-02-08 15:21:04 +0100

    Coef->coef

--- regpy/vecsps/tensor_bases.py
+++ regpy/vecsps/tensor_bases.py
@@ -31,16 +31,16 @@ class TensorBasis(Operator):
         self.ndim = coef_domain.ndim
         self.bases = bases
 
-    def _eval(self, Coef):
+    def _eval(self, coef):
         ## separate 1-D and 2-D because of performance
         if self.ndim == 1 and self.domain[0].size*self.codomain[0].size <= 50000000:
-            return self.bases[0] @ Coef
+            return self.bases[0] @ coef
         elif self.ndim == 1 and (self.domain[0].size+self.domain[1].size)*(self.codomain[0].size+self.codomain[1].size) <= 4000000:
-            return np.linalg.multi_dot([self.bases[0], Coef, self.bases[1].T])
+            return np.linalg.multi_dot([self.bases[0], coef, self.bases[1].T])
         else:
             self.sumrule = "".join(chr(k) for k in range(65,65+self.ndim))+","+",".join(["".join(chr(k) for k in [97+l,65+l]) for l in range(self.ndim)])+"->"+"".join(chr(k) for k in range(97,97+self.ndim))
-            self.einsum_path = np.einsum_path(self.sumrule,Coef,*self.bases, optimize='optimal')[0]
-            return np.einsum(self.sumrule,Coef,*self.bases,optimize=self.einsum_path)
+            self.einsum_path = np.einsum_path(self.sumrule,coef,*self.bases, optimize='optimal')[0]
+            return np.einsum(self.sumrule,coef,*self.bases,optimize=self.einsum_path)
 
     def _adjoint(self, G):
         ## separate 1-D and 2-D because of performance
