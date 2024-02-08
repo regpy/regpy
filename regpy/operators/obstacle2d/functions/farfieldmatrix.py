ed83e6db58ec6d8fbbca6ab8ab4904a77e8b89d0
Jakob Florian Oberender <j.oberender@math.uni-goettingen.de>
2024-02-08 15:08:08 +0100

    weightDL->weight_dl

--- regpy/operators/obstacle2d/functions/farfieldmatrix.py
+++ regpy/operators/obstacle2d/functions/farfieldmatrix.py
@@ -7,22 +7,22 @@ Created on Mon Jul 15 12:19:44 2019
 
 import numpy as np
 
-def farfield_matrix(bd,dire,kappa,weight_sl,weightDL):
+def farfield_matrix(bd,dire,kappa,weight_sl,weight_dl):
     """set up matrix corresponding to the far field evaluation of a combined
-    %single and double layer potential with weigths weight_sl and weightDL, rsp."""
+    %single and double layer potential with weigths weight_sl and weight_dl, rsp."""
 #    print(dire)
     FFmat =  np.pi / (np.size(bd.z,1)*np.sqrt(8*np.pi*kappa)) * np.exp(-complex(0,1)*np.pi/4) \
-         * (weightDL*kappa*dire.T.dot(bd.normal) +complex(0,1)*weight_sl*np.matlib.repmat(bd.zpabs,np.size(dire,1),1)) \
+         * (weight_dl*kappa*dire.T.dot(bd.normal) +complex(0,1)*weight_sl*np.matlib.repmat(bd.zpabs,np.size(dire,1),1)) \
          * np.exp(-complex(0,1)*kappa* (dire.T.dot(bd.z)))
     return FFmat
 
 
-def farfield_matrix_trans(bd,dire,kappa,weight_sl,weightDL):
+def farfield_matrix_trans(bd,dire,kappa,weight_sl,weight_dl):
     """set up matrix corresponding to the far field evaluation of a combined
-    %single and double layer potential with weigths weight_sl and weightDL, rsp.
+    %single and double layer potential with weigths weight_sl and weight_dl, rsp.
     %according to eq. 4.12 by exploiting the linearity of the integral."""
     FFmat_a = 2*np.pi / (np.size(bd.z,1)*np.sqrt(8*np.pi*kappa)) * np.exp(complex(0,1)*np.pi/4) \
-            * (-complex(0,1)*weightDL*kappa*dire.T*bd.normal) \
+            * (-complex(0,1)*weight_dl*kappa*dire.T*bd.normal) \
             * np.exp(-complex(0,1)*kappa* (dire.T * bd.z))
 
     FFmat_b = 2*np.pi / (np.size(bd.z,1)*np.sqrt(8*np.pi*kappa)) * np.exp(complex(0,1)*np.pi/4) \
