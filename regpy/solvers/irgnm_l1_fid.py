a9d06a6d4fb2739bede07ec4fcbf9749f0eab8c4
Jakob Florian Oberender <j.oberender@math.uni-goettingen.de>
2024-02-08 15:40:30 +0100

    _GramX->_gram_x

--- regpy/solvers/irgnm_l1_fid.py
+++ regpy/solvers/irgnm_l1_fid.py
@@ -88,8 +88,8 @@ class IrgnmL1Fid(Solver):
         self.alpha_l1 = alpha_l1
 
         # Computation of some parameters for the iteration
-        self._GramX = self.op.domain.gram(np.eye(len(self.x)))
-        self._GramX = 0.5 * (self._GramX+self._GramX.T)
+        self._gram_x = self.op.domain.gram(np.eye(len(self.x)))
+        self._gram_x = 0.5 * (self._gram_x+self._gram_x.T)
         self._GramY = self.op.codomain.gram(np.eye(len(self.y)))
         self._GramY = 0.5 * (self._GramY+self._GramY.T)
         self._maxiter = 10000
@@ -110,7 +110,7 @@ class IrgnmL1Fid(Solver):
         _, deriv=self.op.linearize(self.x)
         for i in range(len(self.x)):
             self._DF[:,i] = deriv(self._DF[:,i])
-        self._Hess = (np.dot(self._DF,np.linalg.solve(self._GramX, self._DF.T))
+        self._Hess = (np.dot(self._DF,np.linalg.solve(self._gram_x, self._DF.T))
                       + self._regpar*np.linalg.inv(self._GramY))
         self._Hess = 0.5 * (self._Hess+self._Hess.T)
         self._rhs = self.data - self.y - np.dot(self._DF, self.init - self.x)
