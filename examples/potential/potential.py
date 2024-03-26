10d07a77317dbfa840ee5f2fc4058581bde52543
Bjoern Mueller <muellerb@swanl2.server.mps.mpg.de>
2024-03-26 11:08:57 +0100

    Potential examples

--- examples/potential/potential.py
+++ examples/potential/potential.py
@@ -2,20 +2,21 @@ import logging
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 from regpy.solvers.nonlinear.irgnm import IrgnmCG
 from regpy.solvers.nonlinear.newton import NewtonCG
-
 import regpy.stoprules as rules
 from regpy.hilbert import L2, Sobolev
-from regpy.operators.obstacles import Potential
-from regpy.vecsps.obstacles import StarTrigDiscr
+from regpy.vecsps.curves.star_trig import StarTrigDiscr
 from regpy.solvers import HilbertSpaceSetting
+from potential_op import Potential
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(name)-40s :: %(message)s'
 )
 
+"""Forward operator and star-shaped domain"""
 op = Potential(
     domain=StarTrigDiscr(200),
     radius=1.2,
@@ -24,19 +25,24 @@ op = Potential(
 
 setting = HilbertSpaceSetting(op=op, h_domain=Sobolev, h_codomain=L2)
 
+"""Exact data and Poission data"""
 exact_solution = op.domain.sample(lambda t: np.sqrt(3 * np.cos(t)**2 + 1) / 2)
 exact_data = op(exact_solution)
 noise = op.codomain.randn()
 noise = 0.01*setting.h_codomain.norm(exact_data)/setting.h_codomain.norm(noise) * noise
 data = exact_data + noise
 
+"""Initial guess"""
 init = op.domain.sample(lambda t: 1)
 
+"""Solver: NewtonCG or IrgnmCG"""
 solver = NewtonCG(
     setting, data, init = init,
-        cgmaxit=50, rho=0.8
+        cgmaxit=50, rho=0.6
 )
-"""solver = IrgnmCG(
+
+"""
+solver = IrgnmCG(
     setting, data,
     regpar=10,
     regpar_step=0.8,
@@ -44,16 +50,19 @@ solver = NewtonCG(
     cg_pars=dict(
         tol=1e-4
     )
-)"""
+)
+"""
+
 stoprule = (
     rules.CountIterations(100) +
     rules.Discrepancy(
         setting.h_codomain.norm, data,
         noiselevel=setting.h_codomain.norm(noise),
-        tau=2.1
+        tau=1.2
     )
 )
 
+"""Plot function"""
 plt.ion()
 fig, axs = plt.subplots(1, 2)
 axs[0].set_title('Obstacle')
