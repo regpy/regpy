a850328f415569e3e0ed31d8b822d418f9f527f7
Jakob Florian Oberender <j.oberender@math.uni-goettingen.de>
2024-04-11 15:40:25 +0200

    Added interface and manager for parallel computing

--- regpy/operators/parallel_operators.py
+++ regpy/operators/parallel_operators.py
@@ -1,6 +1,7 @@
 from regpy import util, vecsps
 from regpy.operators import Operator
 import multiprocessing as mp 
+from warnings import warn
 from regpy.util import classlogger
 
 class OperatorAsWorker(mp.Process):
@@ -39,7 +40,8 @@ class OperatorAsWorker(mp.Process):
         Raises:
             TypeError: Error is raised if unknown command is received
         """
-        while True:
+        terminate=False
+        while not terminate:#TODO improve error handling
             command = self.conn.recv()
             self.log.debug(self.name+ ' executing '+command[0])
             if command[0] ==  'eval_nodiff':
@@ -58,12 +60,33 @@ class OperatorAsWorker(mp.Process):
                     res = self.deriv.adjoint(command[1])
                 self.conn.send(res)
             elif command[0] == 'break':
-                break
+                terminate=True
             else:
                 raise TypeError(self.name+': unknown command ',command[0])
         self.terminate()
 
-class ParallelVectorOfOperators(Operator):
+class ParallelInterface:
+    def __init__(self,conns,subprocess_count,parallel_manager=None,end_command="break"):
+        self.conns=conns
+        self.subprocess_count=subprocess_count
+        self.end_command=end_command
+        self.parallel_manager=parallel_manager
+        if(self.parallel_manager!=None):
+            self.pid=self.parallel_manager.append(self)
+
+    def terminate_all(self,call_manager=False):
+        for conn in self.conns:
+            conn.send([self.end_command])
+        self.subprocess_count=0
+        if(self.parallel_manager!=None and call_manager):
+            self.parallel_manager.terminated(self.pid)
+
+    def __del__(self):
+        self.terminate_all()
+
+
+
+class ParallelVectorOfOperators(Operator,ParallelInterface):
     """Vector of operators in which all components are evaluated in parallel. 
     The functionality is identical to the sequential analog VectorOfOperators: For
 
@@ -85,7 +108,7 @@ class ParallelVectorOfOperators(Operator):
         Default: vecsps.DirectSum.
     """
 
-    def __init__(self, ops,  domain=None, codomain=None):
+    def __init__(self, ops,  domain=None, codomain=None,parallel_manager=None):
         assert all([isinstance(op, Operator) for op in ops])
         assert ops
 
@@ -105,40 +128,91 @@ class ParallelVectorOfOperators(Operator):
             raise TypeError('codomain={} is neither a VectorSpace nor callable'.format(codomain))
         assert all(op.codomain == c for op, c in zip(ops, codomain))
 
-        self.conn = []
+        conns = []
         it = 0
         for op in ops:
             conn_m, conn_w = mp.Pipe()
-            self.conn.append(conn_m)
+            conns.append(conn_m)
             G = OperatorAsWorker(type(op).__name__+' as worker '+str(it),conn_w,op)
             G.start()
             it += 1
-        super().__init__(domain=self.domain, codomain=codomain, linear=all(op.linear for op in ops))
-    
-    def __del__(self):
-        for conn_m in self.conn:
-            conn_m.send(['break']) 
+        Operator.__init__(self,domain=self.domain, codomain=codomain, linear=all(op.linear for op in ops))
+        ParallelInterface.__init__(self,conns,len(conns),parallel_manager)
 
     def _eval(self, x, differentiate=False):
         if differentiate:
-            for conn_m in self.conn:
+            for conn_m in self.conns:
                 conn_m.send(['eval_diff',x])
         else:
-            for conn_m in self.conn:
+            for conn_m in self.conns:
                 conn_m.send(['eval_nodiff',x])   
-        aux = self.codomain.join(*(conn_m.recv() for conn_m in self.conn))
+        aux = self.codomain.join(*(conn_m.recv() for conn_m in self.conns))
         return aux
 
     def _derivative(self, x):
-        for conn_m in self.conn:
+        for conn_m in self.conns:
                 conn_m.send(['deriv',x])   
-        return self.codomain.join(*(conn_m.recv() for conn_m in self.conn))
+        return self.codomain.join(*(conn_m.recv() for conn_m in self.conns))
 
     def _adjoint(self, y):
         elms = self.codomain.split(y)
         result = self.domain.zeros()    
-        for conn_m, elm in zip(self.conn, elms):
+        for conn_m, elm in zip(self.conns, elms):
             conn_m.send(['adjoint',elm])
             result += conn_m.recv()
         return result
+
+class ParallelExecutionManager:
+    MAX_SUBPROCESSES=16#128
+    total_subprocesses=0
+
+    def __init__(self):
+        self._min_id=0
+        self.managed_ops={}
+        self.managed_processes=0
+
+
+    def check_subprocess_count():
+        if(ParallelExecutionManager.total_subprocesses> ParallelExecutionManager.MAX_SUBPROCESSES):
+            warn(f"Warning: There are already {ParallelExecutionManager.total_subprocesses} subprocesses running.")
+
+    def __enter__(self):
+        ParallelExecutionManager.check_subprocess_count()
+
+    def __exit__(self):
+        self.terminate_all()
+
+    @property
+    def managed_processes(self):
+        return self._managed_processes
+    
+    @managed_processes.setter
+    def managed_processes(self,new_ammount):
+        ParallelExecutionManager.total_subprocesses+=new_ammount-self.managed_processes
+        ParallelExecutionManager.check_subprocess_count()
+        self.managed_processes=new_ammount
+
+    def append(self,parallel_op):
+        assert isinstance(parallel_op,ParallelInterface)
+        id=self._min_id
+        self._min_id+=1
+        self.managed_ops.update({id:(parallel_op,parallel_op.subprocess_count)})
+        self.managed_processes+=parallel_op.subprocess_count
+        return id
+
+    def terminated(self,id):
+        _,subprocess_count=self.managed_ops.pop()
+        self.managed_processes-=subprocess_count
+
+    def terminate_all(self):
+        for id in self.managed_ops.keys():
+            self.managed_ops[id].terminate_all()
+        self.managed_ops.clear()
+        self.managed_processes=0
+        self._min_id=0
+
+
+
+
+
 
