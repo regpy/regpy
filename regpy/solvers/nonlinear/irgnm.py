34ed76c481657928afbe0f285e9e97eb27dc5838
Thorsten Hohage <thohage@gwdg.de>
2024-04-25 00:42:18 +0200

    added Levenberg-Marquardt algorithm

--- regpy/solvers/nonlinear/irgnm.py
+++ regpy/solvers/nonlinear/irgnm.py
@@ -102,6 +102,102 @@ class IrgnmCG(RegSolver):
     def nr_inner_its(self):
         return self._nr_inner_steps
         
+
+class LevenbergMarquardt(RegSolver):
+    r"""The Levenberg-Marquardt method. In each iteration, minimizes
+
+    \[
+        \Vert(x_{n}) + T'[x_n] h - data\Vert^{2} + regpar_{n} \cdot \Vert h\Vert^{2}
+    \]
+
+    where \(T\) is a Frechet-differentiable operator, using `regpy.solvers.linear.tikhonov.TikhonovCG`.
+    \(regpar_n\) is a decreasing geometric sequence of regularization parameters.
+
+    Parameters
+    ----------
+    setting : regpy.solvers.RegularizationSetting
+        The setting of the forward problem.
+    data : array-like
+        The measured data.
+    regpar : float
+        The initial regularization parameter. Must be positive.
+    regpar_step : float, optional
+        The factor by which to reduce the `regpar` in each iteration. Default: \(2/3\).
+    init : array-like, optional
+        The initial guess. Default: the zero array.
+    cg_pars : dict
+        Parameter dictionary for stopping of inner CG iteration passed to the inner `regpy.solvers.linear.tikhonov.TikhonovCG` solver.
+    cg_stop: int
+        Maximum number of inner CG iterations
+    simplified_op : Operator
+        An operator the with the same mapping properties as setting.op, which is cheaper to evaluate. 
+        It is used for the derivative in the Newton equation. 
+        Default: None - then the derivative of setting.op is used.
+    """
+
+    def __init__(
+               self, setting, data, regpar, regpar_step=2 / 3, 
+                 init=None, 
+                 cg_pars={'reltolx': 1/3., 'reltoly': 1/3.,'all_tol_criteria': False}, 
+                cgstop=1000, 
+                inner_it_logging_level = logging.WARNING, 
+                simplified_op = None
+         ):
+        super().__init__(setting)
+        self.data = data
+        """The measured data."""
+        if init is None:
+            init = self.op.domain.zeros()
+        self.init = np.asarray(init)
+        """The initial guess."""
+        self.x = np.copy(self.init)
+        if simplified_op:
+            self.simplified_op = simplified_op
+            _, self.deriv = self.simplified_op.linearize(self.x)
+            self.y = self.op(self.x)
+        else:
+            self.y, self.deriv = self.op.linearize(self.x)
+        self.regpar = regpar
+        """The regularizaton parameter."""
+        self.regpar_step = regpar_step
+        """The `regpar` factor."""
+        self.cg_pars = cg_pars
+        """The additional `regpy.solvers.linear.tikhonov.TikhonovCG` parameters."""
+        self.cgstop = cgstop
+        """Maximum number of iterations for inner CG solver, or None"""
+        self.inner_it_logging_level = inner_it_logging_level
+        self._nr_inner_steps = 0
+
+    def _next(self):
+        if self.cgstop is not None:
+            stoprule = CountIterations(self.cgstop)
+        else:
+            stoprule = CountIterations(2**15)
+        # Disable info logging, but don't override log level for all CountIterations instances.
+        stoprule.log = self.log.getChild('CountIterations')
+        stoprule.log.setLevel(logging.WARNING)
+        # Running Tikhonov solver
+        step, _ = TikhonovCG(
+            setting=RegularizationSetting(self.deriv, self.h_domain, self.h_codomain),
+            data=self.data - self.y,
+            regpar=self.regpar,
+            **self.cg_pars,
+            logging_level = self.inner_it_logging_level
+        ).run(stoprule=stoprule)
+        self.x += step
+        if hasattr(self,'simplified_op'):
+            _, self.deriv = self.simplified_op.linearize(self.x)
+            self.y = self.op(self.x)
+        else:
+            self.y , self.deriv = self.op.linearize(self.x)
+        self.regpar *= self.regpar_step
+        self._nr_inner_steps = stoprule.iteration
+        self.log.info('its.{}: alpha={}, CG its:{}'.format(self.iteration_step_nr,self.regpar,self._nr_inner_steps))
+    
+    def nr_inner_its(self):
+        return self._nr_inner_steps
+
+
 from regpy.operators import MatrixMultiplication
 from regpy import util
 from scipy.sparse.linalg import eigsh
