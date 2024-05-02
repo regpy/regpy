a35528307a78b8201f108f5fb716f065435b50f1
ehlers26 <b.ehlers@math.uni-goettingen.de>
2024-05-02 15:30:12 +0200

    #35 updated potential test

--- tests/example_for_tests/potential.py
+++ tests/example_for_tests/potential.py
@@ -1,16 +1,14 @@
 import logging
 
+
 import numpy as np
-from regpy.solvers.nonlinear.irgnm import IrgnmCG
+
 from regpy.solvers.nonlinear.newton import NewtonCG
 
 import regpy.stoprules as rules
 from regpy.hilbert import L2, Sobolev
-
-from examples.potential.potential import Potential
-from regpy.vecsps import UniformGridFcts
-from regpy.vecsps.curve import StarTrigDiscr
 from regpy.solvers import RegularizationSetting
+from potential import Potential
 
 
 
@@ -19,46 +17,54 @@ def test_potential():
         level=logging.INFO,
         format='%(asctime)s %(levelname)s %(name)-40s :: %(message)s'
     )
-    #N_means = 128
+
+    #Forward operator
     op = Potential(
-     #   domain=StarTrigDiscr(200),
-     #   codomain=UniformGridFcts(np.linspace(0, 2*np.pi, N_means, endpoint=False), dtype=complex),
-        radius=1.2
+        radius=1.3
     )
 
     setting = RegularizationSetting(op=op, penalty=Sobolev, data_fid=L2)
 
-    exact_solution = op.domain.sample(lambda t: np.sqrt(3 * np.cos(t)**2 + 1) / 2)
+    #Exact data and Poission data
+    exact_solution = op.domain.sample(lambda t: np.sqrt(3*np.cos(t)**2+1)/2)
     exact_data = op(exact_solution)
     noise = op.codomain.randn()
-    noise = 0.01*setting.h_codomain.norm(exact_data)/setting.h_codomain.norm(noise) * noise
+    noise = 0.01*setting.h_codomain.norm(exact_data)/setting.h_codomain.norm(noise)*noise
     data = exact_data + noise
 
+    #Initial guess
     init = op.domain.sample(lambda t: 1)
 
+    #Solver: NewtonCG or IrgnmCG
     solver = NewtonCG(
         setting, data, init = init,
-            cgmaxit=50, rho=0.8
+            cgmaxit=50, rho=0.6
     )
-    """solver = IrgnmCG(
+
+    """
+    solver = IrgnmCG(
         setting, data,
-        regpar=10,
-        regpar_step=0.8,
-        init=init,
-        cg_pars=dict(
-            tol=1e-4
+        regpar = 1,
+        regpar_step = 0.5,
+        init = init,
+        cg_pars = dict(
+            tol = 1e-4
         )
-    )"""
+    )
+    """
     stoprule = (
         rules.CountIterations(100) +
         rules.Discrepancy(
             setting.h_codomain.norm, data,
-            noiselevel=setting.h_codomain.norm(noise),
+            noiselevel = setting.h_codomain.norm(noise),
             tau=2.1
         )
     )
 
+    #Plot function
 
-    for n, (reco, reco_data) in enumerate(solver.until(stoprule)):
-        pass
+
+    solver.run(stoprule)
+
+
 
