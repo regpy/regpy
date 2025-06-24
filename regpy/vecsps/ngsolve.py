ef6ba8846feeec0eac0603f509f4def5a8682699
Philipp Mickan <p.mickan@math.uni-goettingen.de>
2025-06-24 09:43:24 +0200

    Small change in construction of codimension and utility space in NgsSpaces

--- regpy/vecsps/ngsolve.py
+++ regpy/vecsps/ngsolve.py
@@ -33,8 +33,8 @@ class NgsSpace(VectorSpace):
         from netgen.libngpy._meshing import NgException
         try:
             self.codim = len(fes.components)
-            assert self.codim == fes.mesh.dim
-            self._fes_util = ngs.VectorL2(self.fes.mesh, order=0, complex = self.is_complex)
+            # assert self.codim == fes.mesh.dim
+            self._fes_util = ngs.L2(self.fes.mesh, order=0, complex = self.is_complex)**self.codim
         except NgException:
             self.codim = 1
             self._fes_util = ngs.L2(self.fes.mesh, order=0, complex = self.is_complex)
@@ -48,7 +48,10 @@ class NgsSpace(VectorSpace):
             return NotImplemented
 
     def ones(self):
-        self._gfu_fes.Set(1)
+        if self.codim == 1:
+            self._gfu_fes.Set(1)
+        else:
+            self._gfu_fes.Set(tuple(1 for _ in range(self.codim)))
         return self._gfu_fes.vec.FV().NumPy().copy()
 
     def rand(self, rand=np.random.random_sample):
