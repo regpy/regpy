7b56d9d4d669c4b9d7149d43d9ef9342eb09c5c7
Philipp Mickan <p.mickan@math.uni-goettingen.de>
2025-07-01 16:48:55 +0200

    pulled new version of examples and removed volterra test from operator tests

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -3,20 +3,6 @@ from regpy.operators import *
 from regpy.operators.convolution import *
 import regpy.util.operator_tests as ot
 from regpy import vecsps
-from examples.volterra import volterra 
-
-
-def test_volterra():
-    #linear
-    op=volterra.Volterra(domain=vecsps.UniformGridFcts(np.linspace(0, 2 * np.pi, 10)))
-    ot.test_operator(op)
-    #nonlinear
-    op=volterra.Volterra(domain=vecsps.UniformGridFcts(np.linspace(0, 2 * np.pi, 10)),exponent=3)
-    ot.test_operator(op)
-    #extra: adjoint derivative of composition
-    op=volterra.Volterra(domain=vecsps.UniformGridFcts(np.linspace(0, 2 * np.pi, 10)),
-                         exponent=3) *volterra.Volterra(domain=vecsps.UniformGridFcts(np.linspace(0, 2 * np.pi, 10)),exponent=2)
-    ot.test_adjoint_derivative(op)
 
 def test_identity():
     #real
