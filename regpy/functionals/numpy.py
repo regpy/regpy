added new tests for second derivative (by finite differences) and for Lipschitz constants and convexity parameters. Also added tests for shifts, dilations, linear combinations, etc. Fixed scaling error for conj_hessian.

regpy/functionals/numpy.py
@@ -103,7 +103,9 @@ class IntegralFunctionalBase(Functional):
         return self._f_conj_deriv(vstar/self.domain.measure,**self.kwargs)
     
     def _conj_hessian(self, vstar):
-        return PtwMultiplication(self.domain,self._f_conj_second_deriv(vstar/self.domain.measure,**self.kwargs))
+        return PtwMultiplication(self.domain,
+                                 self._f_conj_second_deriv(vstar/self.domain.measure,**self.kwargs)/self.domain.measure
+                                 )
 
     def _f(self,v,**kwargs):
         raise NotImplementedError
@@ -466,8 +468,9 @@ class L1MeasureSpace(IntegralFunctionalBase):
     domain : regpy.vecsps.MeasureSpaceFcts
         Domain on which to define the generic L1.
     """
-    def __init__(self, domain,**kwargs):
-        super().__init__(domain,**kwargs)
+    def __init__(self, domain,eps=1e-10,**kwargs):
+        self.eps=eps
+        super().__init__(domain,conj_dom_u=1.,conj_dom_l=-1.,**kwargs)
 
     def _f(self, v,**kwargs):
         return np.abs(v)
@@ -501,14 +504,14 @@ class L1MeasureSpace(IntegralFunctionalBase):
         return res
     
     def _f_conj_deriv(self, v_star,**kwargs):
-        if np.max(np.abs(v_star))>1:
-            raise NotInEssentialDomainError()
+        if np.max(np.abs(v_star))>1+self.eps:
+            raise NotInEssentialDomainError(f"Argument too large by factor {np.abs(v_star)}.")
         else:
             return np.zeros_like(v_star)
 
     def _f_conj_second_deriv(self, v_star,**kwargs):
-        if np.max(np.abs(v_star))>=1:
-            raise NotTwiceDifferentiableError('L1')
+        if np.max(np.abs(v_star))>=1+self.eps:
+            raise NotInEssentialDomainError(f"Argument too large by factor {np.abs(v_star)}.")
         else:
             return self.domain.zeros()
 
@@ -831,7 +834,7 @@ class Huber(IntegralFunctionalBase):
         Only used for conjugate functional. See description of `QuadraticIntv`
     """
 
-    def  __init__(self, domain,as_primal=True,sigma = 1.,eps=0.,**kwargs):
+    def  __init__(self, domain,as_primal=True,sigma = 1.,eps=1e-10,**kwargs):
         assert isinstance(sigma, (float,int)) or sigma in domain
         if isinstance(sigma, (float,int)) :
             self.sigma = np.broadcast_to(np.real(sigma),domain.shape)
@@ -916,9 +919,10 @@ class QuadraticIntv(IntegralFunctionalBase):
         or NotInEssentialDomain exceptions in the presence of rounding errors
     """
 
-    def  __init__(self, domain,as_primal=True,sigma=1.,eps=0.,**kwargs):
+    def  __init__(self, domain,as_primal=True,sigma=1.,eps=1e-10,**kwargs):
         assert isinstance(sigma, (float,int)) or sigma in domain 
         assert np.min(sigma)>0
+        self.eps=eps
         if isinstance(sigma, (float,int)):
             self.sigma = np.broadcast_to(np.real(sigma), domain.shape)
             self.sigmaeps = np.broadcast_to(sigma*(1+eps), domain.shape)           
@@ -945,8 +949,8 @@ class QuadraticIntv(IntegralFunctionalBase):
     def _f_deriv(self, u,**kwargs):
         self._aux = np.abs(u)
         self._aux /= self.sigmaeps
-        if np.max(self._aux)>1.:
-            raise NotInEssentialDomainError(f'QuadraticIntv. Argument too large by factor {np.max(self._aux)}')
+        if np.max(self._aux)>1.+self.eps:
+            raise NotInEssentialDomainError(f'QuadraticIntv. Argument too large by factor 1+{np.max(self._aux)-1:e}. eps={self.eps:e}.')
         return u.copy()
 
     def _f_prox(self,u,tau,**kwargs):
@@ -958,8 +962,8 @@ class QuadraticIntv(IntegralFunctionalBase):
     def _f_second_deriv(self, u,**kwargs):
         self._aux = np.abs(u)
         self._aux /= self.sigmaeps
-        if np.max(self._aux)>=1.:
-            raise NotTwiceDifferentiableError('QuadraticIntv')
+        if np.max(self._aux)>=1.+self.eps:
+            raise ValueError(f'Argument not in essential domain. Too large by a factor of 1+{np.max(self._aux)-1:e}. eps={self.eps:e}')
         else:
             return np.ones_like(u)
 
@@ -1078,7 +1082,7 @@ class QuadraticBilateralConstraints(LinearCombination):
         If constraints are violated by less then eps times the interval width, the polynomial is evaluated, rather than returning np.inf.
     """
 
-    def __init__(self,domain, lb=None, ub=None, x0=None,alpha=1.,eps=0.,**kwargs):
+    def __init__(self,domain, lb=None, ub=None, x0=None,alpha=1.,**kwargs):
         assert isinstance(domain,MeasureSpaceFcts)
         if isinstance(lb,(float,int)):
             lb = lb*domain.ones()
@@ -1099,7 +1103,7 @@ class QuadraticBilateralConstraints(LinearCombination):
         assert isinstance(alpha,(float,int))
 
         self.lb = lb; self.ub = ub; self.x0 =x0; self.alpha = alpha
-        F = QuadraticIntv(domain,sigma=(ub-lb)/2.,eps=eps,**kwargs)
+        F = QuadraticIntv(domain,sigma=(ub-lb)/2.,**kwargs)
         center = (ub+lb)/2
         lin = LinearFunctional(center-x0,
                             domain=domain,

regpy/util/functional_tests.py
@@ -1,5 +1,5 @@
 from regpy.functionals import IntegralFunctionalBase
-from regpy.functionals.base import Conj
+from regpy.functionals.base import Conj, NotTwiceDifferentiableError
 from random import uniform
 import numpy as np
 
@@ -104,7 +104,42 @@ def test_subgradient(func,u=None,v=None,v_length=1e-5,tolerance=1e-10):
     grad_u=func.subgradient(u)
     err=func(u)-func(v)+(func.domain.vdot(grad_u,v-u)).real
     assert err<tolerance,f'err={err}'
-    
+
+def test_second_derivative(func,u=None,h=None,eps=1e-8,tolerance = 1e-2,abs_tol=1e-6):
+    if u is None :
+        if func.separable:
+            u=sample_essential_domain(func)
+        else:
+            u=func.domain.randn()
+    if h is None:
+        h = - func.subgradient(u)
+        if func.separable:
+            h[u+eps*h>=func.dom_u] *= -1.
+            h[u+eps*h<=func.dom_l] *= -1.
+            h[u+eps*h>=func.dom_u] = 0.
+            h[u+eps*h<=func.dom_l] = 0.          
+    func_pp = func.hessian(u)(h)
+    diffq = (func.subgradient(u+eps*h)-func.subgradient(u))/eps
+    err = np.linalg.norm(func_pp-diffq)/(1e-14+np.linalg.norm(func_pp))
+    #print('err second der: ',err,'rel.err',(func_pp-diffq)/func_pp) #'diffq',diffq,'u',u,'h',h)
+    assert np.linalg.norm(func_pp-diffq)<=np.max([abs_tol,tolerance * np.linalg.norm(func_pp)]), f"err: {err}, tol: {tolerance}, norm second deriv. {np.linalg.norm(func_pp)}"
+
+def test_Lipschitz_convexity(func,u=None,safety=1.5):
+    assert func.separable
+    if u is None :
+        u=sample_essential_domain(func)
+    fpp = func.h_domain.gram_inv(func.hessian(u)(func.domain.ones()))
+    #print('Lipschitz:', func.Lipschitz,np.max(fpp))
+    #print('convexity:', func.convexity_param,np.min(fpp))
+    assert func.Lipschitz>=np.max(fpp)
+    if np.max(func.dom_u)<np.infty or np.min(func.dom_l)>-np.infty:
+        assert func.Lipschitz==np.infty, "Lipschitz constant finite, but essential domain is constrained."
+    if func.Lipschitz<np.infty:
+        assert func.Lipschitz<=safety*np.max(fpp), f"Lipschitz constant {func.Lipschitz/np.max(fpp)} times larger than estimate based on second derivative. Safety ={safety}."
+    assert func.convexity_param<=np.min(fpp)
+    if func.convexity_param>0:
+        assert safety*func.convexity_param>=np.min(fpp), f"convexity parameter {np.min(fpp)/func.convexity_param} times smaller than estimate based on second derivative. Safety={safety}."
+
 # def test_subgradient_and_conj(func,u=None,eps=1e-10):
 #     if(u is None):
 #         u=func.domain.randn()
@@ -150,7 +185,11 @@ def test_young_equality(func,u=None,tolerance=1e-10):
     err=abs(t1-t2-t3)/np.max(np.abs([1e-14,t1,t2,t3]))
     assert err<tolerance,f'err={err}, F(u)={t2}, F^*(u)={t3}, <u,grad_u>={t3}'
 
-def test_functional(func,u_s=None,sample_N=5,test_conj=True,u_stars=None,sample_conj_N=5,print_results=False,tolerance=1e-10):
+def test_functional(func,u_s=None,sample_N=5,
+                    test_conj=True,
+                    u_stars=None,sample_conj_N=5,print_results=False,
+                    test_second_deriv = True, test_second_deriv_conj = True,
+                    tolerance=1e-10):
     r"""Runs all implemented tests for a given functional. By default tests that cannot be verified because of 
     missing implementations are ignored.
 
@@ -183,7 +222,7 @@ def test_functional(func,u_s=None,sample_N=5,test_conj=True,u_stars=None,sample_
         If the test fails.
     """
     if (u_s is None):
-        if isinstance(func,IntegralFunctionalBase) or (isinstance(func,Conj) and isinstance(func.func,IntegralFunctionalBase)):
+        if func.separable:
             u_s= [sample_essential_domain(func)]
         else:
             u_s = [func.domain.randn() for _ in range(sample_N)]
@@ -206,6 +245,20 @@ def test_functional(func,u_s=None,sample_N=5,test_conj=True,u_stars=None,sample_
         except(NotImplementedError):
             if(print_results):
                 print('Young equality could not be checked because of missing implementation')
+        if test_second_deriv:
+            try:
+                test_second_derivative(func,u)
+            except (NotTwiceDifferentiableError, NotImplementedError):
+                if (print_results):
+                    print('Second derivative could not be tested as functiional is not twice differentiable.')
+            if func.separable:
+                try:
+                    test_Lipschitz_convexity(func)
+                except (NotImplementedError):
+                    if print_results:
+                        print('Lipschitz constant and convexity parameter could not be checked because of missing implementation.')
     if(test_conj):
-        test_functional(func.conj,u_s=u_stars,sample_N=sample_conj_N,test_conj=False,print_results=print_results,tolerance=tolerance)
+        test_functional(func.conj,u_s=u_stars,sample_N=sample_conj_N,
+                        test_conj=False,test_second_deriv=test_second_deriv_conj, 
+                        print_results=print_results,tolerance=tolerance)
                 

tests/functional_tests/numpy_functional.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from regpy.vecsps import NumPyVectorSpace, MeasureSpaceFcts,UniformGridFcts
 from regpy.functionals import *
+from regpy.functionals.base import HorizontalShiftDilation, LinearFunctional
 from regpy.hilbert import L2
 from regpy.util import functional_tests as ft
 
@@ -12,12 +13,14 @@ def test_L1():
     assert (func(x) == 50.0)
     for tau in [0.1,1,2]:
         assert (func.proximal(x,tau) == np.maximum(0, np.abs(x)-tau)*np.sign(x)).all()
-    ft.test_functional(func)
+    ft.test_functional(func,test_second_deriv=False)
     dom=MeasureSpaceFcts(measure=np.array([[1,2,3],[4,5,6]],dtype=np.float64))
     x = dom.ones()
     func = L1(dom)
     assert (func(x) == 21.0)
-    ft.test_functional(func,u_s=[func.domain.rand() for _ in range(5)],u_stars=[func.domain.rand() for _ in range(5)])
+    ft.test_functional(func,u_s=[func.domain.rand() for _ in range(5)],
+                       u_stars=[func.domain.rand() for _ in range(5)],
+                       test_second_deriv=False)
 
 # def test_TV():
 #     ugf = UniformGridFcts((-1,1,10),(-1,1,10))
@@ -29,10 +32,18 @@ def test_kullback_leibler():
     F=KL(dom,w=dom.ones())
     ft.test_functional(F)
 
+    ft.test_functional(HorizontalShiftDilation(F,dilation=3,shift=F.domain.ones()))
+    ft.test_functional(F-2.)
+    ft.test_functional(F+LinearFunctional(F.domain.rand(),domain=F.domain))
+
 def test_relative_entropy():
     dom=UniformGridFcts((-5,7,4),(100,200,3))
     F=RE(dom,w=dom.ones())
     ft.test_functional(F)
+
+    ft.test_functional(3.*F)
+    ft.test_functional(F+LinearFunctional(F.domain.rand(),domain=F.domain))
+
 
 def test_huber():
     dom=MeasureSpaceFcts(measure=np.array([[1,2,3],[4,5,6]],dtype=np.float64),dtype=np.complex128)
@@ -40,6 +51,12 @@ def test_huber():
     sigma[0,0]=4
     F=Hub(dom,sigma=sigma,eps=1e-10)
     ft.test_functional(F)
+
+    ft.test_functional(HorizontalShiftDilation(F,dilation=3,shift=F.domain.ones()))
+    ft.test_functional(F-2.)
+    ft.test_functional(F+LinearFunctional(F.domain.rand(),domain=F.domain),
+                       test_second_deriv=False,test_second_deriv_conj=False
+                       )    
 
 def test_quadratic_intv():
     dom=MeasureSpaceFcts(measure=np.array([[1,2,3],[4,5,6]],dtype=np.float64),dtype=np.complex128)
@@ -81,7 +98,7 @@ def test_quadratic_positive_semidef():
     F_tr=QuadPosSemi(dom,trace_val=2,tol=1e-10)
     u_s_tr=[2*u/np.trace(u) for u in u_s]
     assert np.abs(0.5*np.sum((2*diags[0]/np.sum(diags[0]))**2)-F_tr(u_s_tr[0]))<1e-10
-    ft.test_functional(F_tr,u_s_tr)
+    ft.test_functional(F_tr,u_s_tr,test_second_deriv=False)
 
 def test_hilbertnorm():
     dom=MeasureSpaceFcts(measure=np.array([[1,2,3],[4,5,6]],dtype=np.float64),dtype=np.complex128)