Fixed small bugs in numerical prox approximation. Extended tests for Lpp work now.

regpy/functionals/numpy.py
@@ -570,13 +570,14 @@ class IntegralFunctionalBase(Functional):
     def _f_second_deriv(self,v,**kwargs):
         raise NotImplementedError
 
-    def _f_prox(self,v,tau,tol=1e-12, maxNewtonIter=15,maxBisecIter=300,maxBoundsIter=100,**kwargs):
+    def _f_prox(self,v,tau,tol=1e-12, abstol=1e-12,maxNewtonIter=15,maxBisecIter=300,maxBoundsIter=100,**kwargs):
         if self.__class__.__dict__.get("_f_deriv") is not None:
             vclip = np.minimum(v,self.dom_u)
             vclip = np.maximum(vclip,self.dom_l)
             f_second_deriv = self._f_second_deriv if self._f_second_deriv is not None else None
             return self._numerical_prox(v,tau,self._f_deriv,f_second_deriv,self.dom_l, self.dom_u, 
-                                        tol=tol,maxNewtonIter=maxNewtonIter,maxBisecIter=maxBisecIter,maxBoundsIter=maxBoundsIter,
+                                        tol=tol,abstol=abstol,
+                                        maxNewtonIter=maxNewtonIter,maxBisecIter=maxBisecIter,maxBoundsIter=maxBoundsIter,
                                         **kwargs
                                         )
         else:
@@ -591,30 +592,31 @@ class IntegralFunctionalBase(Functional):
     def _f_conj_second_deriv(self,vstar,**kwargs):
         raise NotImplementedError
 
-    def _f_conj_prox(self,vstar,tau,tol=1e-12, maxNewtonIter=15,maxBisecIter=300,maxBoundsIter=100,**kwargs):
+    def _f_conj_prox(self,vstar,tau,tol=1e-12, abstol=1e-12, 
+                     maxNewtonIter=15,maxBisecIter=300,maxBoundsIter=100,
+                     **kwargs
+                     ):
         if self.__class__.__dict__.get("_f_conj_deriv") is not None:
-            print('check existence of _conj_prox',self.__class__.__dict__.get("_f_conj_prox") is not None)
             vclip = np.minimum(vstar,self.conj_dom_u)
             vclip = np.maximum(vclip,self.conj_dom_l)
             vclip /= self.measure
-            #self._f_conj_deriv(vclip,**kwargs)
-            #self._f_conj_second_deriv(vclip,**kwargs)
             f_conj_second_deriv = self._f_conj_second_deriv if self._f_conj_second_deriv is not None else None
             return self._numerical_prox(vstar,tau,
                                         self._f_conj_deriv,f_conj_second_deriv,
                                         self.conj_dom_l/self.measure, 
                                         self.conj_dom_u/self.measure, 
-                                        tol=tol,maxNewtonIter=maxNewtonIter,maxBisecIter=maxBisecIter,maxBoundsIter=maxBoundsIter,
+                                        tol=tol, abstol=abstol,
+                                        maxNewtonIter=maxNewtonIter,maxBisecIter=maxBisecIter,maxBoundsIter=maxBoundsIter,
                                         **kwargs
                                         )
         else:
-        #except NotImplementedError:
             NotImplementedError('Need first derivative of conjugate functional for numerical conjugate prox operator')
 
-    def _numerical_prox(self, y, tau, 
-                        fp, fpp, dom_l, dom_u, 
+    def _numerical_prox(self, y, tau, fp, fpp, 
+                        dom_l, dom_u, 
                         start=None,ub=None, lb=None,
-                        tol=1e-12, maxNewtonIter=15,maxBisecIter=300,maxBoundsIter=100,**kwargs):
+                        tol=1e-15, abstol=1-15, 
+                        maxNewtonIter=15,maxBisecIter=300,maxBoundsIter=100,**kwargs):
         """
         Vectorized proximal operator of a smooth convex scalar function
         using Newton's method with automatic fallback to bisection.
@@ -649,9 +651,9 @@ class IntegralFunctionalBase(Functional):
         
         assert isinstance(y,np.ndarray)
         if np.isscalar(dom_u):
-            dom_u = np.full_like(y,dom_u)
+            dom_u = np.broadcast_to(dom_u,y.shape)
         if np.isscalar(dom_l):
-            dom_l = np.full_like(y,dom_l)    
+            dom_l = np.broadcast_to(dom_l,y.shape)    
         if not np.all(dom_l<=dom_u):
             raise ValueError('Upper/lower bounds of essential domain invalid.')
   
@@ -663,7 +665,7 @@ class IntegralFunctionalBase(Functional):
 
 
         r = fp(x,**kwargs) + (x - y)/tau
-        converged = (dom_u-dom_l<tol) # width of essential domain <tol
+        converged = ((dom_u-dom_l<abstol) | (dom_u-dom_l < tol * np.maximum(dom_u,-dom_l)))   # width of essential domain <tol
         converged = converged | (r==0) # optimality condition satisfied 
         converged = converged | ((x==dom_l) & (r>=0)) # optimality condition at left boundary satisfied
         converged = converged | ((x==dom_u) & (r<=0)) # optimality condition at right boundary satiesfied
@@ -682,37 +684,32 @@ class IntegralFunctionalBase(Functional):
 #            ub[x_too_small] = np.minimum(x[x_too_small] 
 #                                      + np.minimum(np.ones_like(r[x_too_small]), - tau*r[x_too_small]),
 #                                        dom_u[x_too_small]),
-                                    
+
         lb = x.copy()
         x_too_large = (r>0) & (~converged)
         if np.any(x_too_large):
-           lb[x_too_large] = np.maximum(x[x_too_large]
-                                     - np.minimum(tau*np.ones_like(r[x_too_large]), tau*r[x_too_large]),
-                                        dom_l[x_too_large])
-        """
-        if lb is None:
-            lb = np.minimum(x,dom_u)-1.
-        lb[lb==-np.inf]=x[lb==-np.inf]-1.
-        lb = np.maximum(lb,dom_l)
-        print(f'lb corrected: {lb}')
-        if ub is None:
-            ub = np.maximum(x,dom_l)+1.
-        ub[ub==np.inf]=x[ub==np.inf]+1.
-        ub = np.minimum(ub,dom_u)
-        print(f'ub corrected: {ub}')
-        """
+           xl = x[x_too_large]
+           rl = r[x_too_large]
+           dl = dom_l[x_too_large]
+           up = np.minimum(tau*np.ones_like(xl),tau*rl)
+           up2 = np.maximum(xl-up,dl)
+           lb[x_too_large] = up2
+#           lb[x_too_large] = np.maximum(x[x_too_large]
+#                                     - np.minimum(tau*np.ones_like(r[x_too_large]), tau*r[x_too_large]),
+#                                        dom_l[x_too_large])
 
-        #self.log.debug(f'before correction: r: {r}\n ub: {ub}\n lb: {lb}\n diff: {ub-lb}')
+        self.log.debug(f'before correction: r: {r}\n ub: {ub}\n lb: {lb}\n diff: {ub-lb}')
         if not np.all(lb<=ub):
             ind = lb>ub
             raise RuntimeError(f'lb<=ub violated for input values {y[lb>ub]}. lb: {lb[lb>ub]}, ub: {ub[lb>ub]} ')
+
         # decrease lb where necessary to make it a valid lower bound
         for it in range(maxBoundsIter):
             v = fp(lb,**kwargs) + (lb- y)/tau            
-            ind = (v>0) & ~converged
-            if it==0 and not np.all(ub[ind]-lb[ind]>tol):
-                ii = ind & (ub-lb<=tol)
-                lb[ii] = np.maximum(lb[ii]-tol,dom_l[ii])
+            ind = (v>0) & (~converged) & (lb>dom_l)
+            if it==0 and not np.all(ub[ind]-lb[ind]>tol*np.maximum(ub[ind],-lb[ind])):
+                ii = ind & ((ub-lb<=tol * np.maximum(ub,-lb)))
+                lb[ii] = np.maximum(lb[ii]-tol*np.abs(lb[ii])-abstol,dom_l[ii])
                 self.log.debug(f'adding {np.sum(ii & (lb==dom_l))} indices as converged.')
                 converged = converged | (ii & (lb==dom_l))
             self.log.debug(f'lower bound it {it}: {np.sum(ind)} indices invalid.')
@@ -721,7 +718,7 @@ class IntegralFunctionalBase(Functional):
             else:
                 lb[ind] = np.maximum(dom_l[ind],2*lb[ind]-ub[ind])
         if not np.sum(ind)==0:
-            raise RuntimeError("Could not determine lower bound. Increase maxBoundsIter!") 
+            raise RuntimeError(f"Could not determine lower bound. Increase maxBoundsIter! lb: {lb[ind]}, ub: {ub[ind]}, dom_l: {dom_l[ind]}") 
         r = fp(lb,**kwargs) + (lb - y)/tau 
         converged = converged | ((lb==dom_l) & (r>=0))
         if not np.all((r<=0) | converged):
@@ -730,11 +727,11 @@ class IntegralFunctionalBase(Functional):
         # increase ub where necessary to make it a valid upper bound
         for it in range(maxBoundsIter):
             v = fp(ub,**kwargs) + (ub- y)/tau            
-            ind = (v<0) & ~converged
+            ind = (v<0) & (~converged)
             self.log.debug(f'upper bound it {it}: {np.sum(ind)} indices invalid')
-            if it==0 and not np.all(ub[ind]-lb[ind]>tol):
-                ii = ind & (ub-lb<=tol)
-                ub[ii] = np.minimum(dom_u[ii],lb[ii]+tol)
+            if it==0 and not np.all(ub[ind]-lb[ind]>tol*np.maximum(ub[ind],-lb[ind])):
+                ii = ind & (ub-lb<=tol * np.maximum(ub,-lb))
+                ub[ii] = np.minimum(dom_u[ii],ub[ii]+tol*np.abs(ub[ii])+abstol)
                 self.log.debug(f'adding {np.sum(ii & (ub==dom_u))} indices as converged.')
                 converged = converged | (ii & (ub==dom_u))
             if np.sum(ind)==0:
@@ -772,10 +769,10 @@ class IntegralFunctionalBase(Functional):
                 ub[r<0]  = np.minimum(ub[r<0],x[r<0] - tau*r[r<0])
                 # Check convergence
                 #conv = np.abs(r) < tol
-                conv = np.abs(ub-lb) < tol
+                conv = ((np.abs(ub-lb) < abstol) | (ub-lb <=tol*np.maximum(ub,-lb)))
                 converged = converged | conv
                 x = np.where(conv, x, xnew)
-                self.log.debug(f'Newton it {iter}: {np.sum(converged==True)} out of {len(x)} converged.')
+                self.log.debug(f'Newton it {iter}: {np.sum(converged==True)} out of {len(x.flatten())} converged.')
     
                 if np.all(converged):
                     return x
@@ -798,12 +795,15 @@ class IntegralFunctionalBase(Functional):
                 right = fM > 0
                 ub = np.where(right, Mi, ub)
                 lb = np.where(~right, Mi, lb)
-                self.log.debug(f'bisection it. {iter}: {np.sum(ub-lb>tol)} not converged')
-                if np.all((ub-lb) < tol):
+                nr_not_converged = np.sum(((ub-lb) >= abstol) & ((ub-lb) >= tol*np.maximum(ub,-lb)))
+                self.log.debug(f'bisection it. {iter}: {nr_not_converged} not converged')
+                if nr_not_converged == 0:
                     break
             x[mask] = 0.5*(lb+ub)
-            if np.max(ub-lb)>tol:
-                raise RuntimeError('Could not satisfy tolerance criterium in bisection algorithm.')
+            if np.any((ub-lb>=abstol)  &  ((ub-lb) >= tol*np.maximum(ub,-lb))):
+                ind = (ub-lb>= abstol) &  ((ub-lb) >= tol*np.maximum(ub,-lb))
+                ff =fp(x[mask],mask=mask,**kwargs) + (x[mask]-y[mask])/tau
+                raise RuntimeError(f'Could not satisfy either of the tolerance criteria (tol = {tol}, abstol = {abstol}) after {iter} steps of bisection algorithm at indices {np.where(ind)}. x values: {x[mask][ind]}, f values: {ff[ind]}, ub-lb {ub[ind]-lb[ind]}.')
 
         return x        
 
@@ -899,7 +899,7 @@ class LppPower(IntegralFunctionalBase):
             return v/(1+tau)
         else:
             return self._numerical_prox(v,tau,
-                                self._f_deriv,self._f_second_deriv,self.dom_l, self.dom_u, 
+                                self._f_deriv,self._f_second_deriv,-np.inf, np.inf,
                                 tol=1e-12,maxNewtonIter=10,maxBisecIter=300,maxBoundsIter=300,
                                 **kwargs
                                 )
@@ -937,8 +937,7 @@ class LppPower(IntegralFunctionalBase):
         else:
             return self._numerical_prox(v_star,tau,
                                 self._f_conj_deriv,self._f_conj_second_deriv,
-                                np.broadcast_to(-inf,self.domain.shape), 
-                                np.broadcast_to(inf,self.domain.shape), 
+                                -np.inf, np.inf, 
                                 tol=1e-12,maxNewtonIter=10,maxBisecIter=300,maxBoundsIter=300,
                                 **kwargs
                                 )

tests/functional_tests/numpy_functional.py
@@ -10,16 +10,21 @@ from regpy.util import functional_tests as ft
 def test_Lpp():
     dom = UniformGridFcts((-3.,3.,100))
     x = np.linspace(-3.,3.,100)
-    #Numerical prox does not yet work together with options quad_taylor_x and lin_taylor_x!
-    #for p in [1.5,2.,2.5]:
-    #    for (l,u) in [(0.1,2.3),(-2.1,-1.2),(-1.2,1.)]:
-    #        #print('p=',p,'l=',l,'u=',u)
-    #        ft.test_functional(Lpp(dom,p=p,constr_l=l,quad_taylor_u=u))
-    #        ft.test_functional(Lpp(dom,p=p,quad_taylor_l=l,lin_taylor_u=u))
-    #        ft.test_functional(Lpp(dom,p=p,lin_taylor_l=l,constr_u=u))
-    #        ft.test_functional(Lpp(dom,p=p,quad_taylor_l=l,quad_taylor_u=u))
-    ft.test_functional(Lpp(dom,p=1.5,constr_l=-2,quad_taylor_u=3.),test_second_deriv_conj=False)
-    ft.test_functional(Lpp(dom,p=2.5,quad_taylor_l=1.1,quad_taylor_u=2.),test_second_deriv=False)
+    for p in [1.5,2.,2.5]:
+        for (l,u) in [(0.1,2.3),(-2.1,-1.2),(-1.2,1.)]:
+            print('p=',p,'l=',l,'u=',u)
+            ft.test_functional(Lpp(dom,p=p,constr_l=l,quad_taylor_u=u),
+                               test_second_deriv=(p>=2),test_second_deriv_conj=False
+                               )
+            ft.test_functional(Lpp(dom,p=p,quad_taylor_l=l,lin_taylor_u=u),
+                               test_second_deriv=(p>=2),test_second_deriv_conj=(p<=2)
+                               )
+            ft.test_functional(Lpp(dom,p=p,lin_taylor_l=l,constr_u=u),
+                               test_second_deriv=False,test_second_deriv_conj=False
+                               )
+            ft.test_functional(Lpp(dom,p=p,quad_taylor_l=l,quad_taylor_u=u),
+                               test_second_deriv=(p>=2),test_second_deriv_conj=(p<=2)
+                               )
 
 def test_L1():
     dom = NumPyVectorSpace((2,10)) 
@@ -43,24 +48,25 @@ def test_L1():
 #     ft.test_functional(func,u_s=[func.domain.rand() for _ in range(5)],u_stars=[func.domain.rand() for _ in range(5)])
 
 def test_kullback_leibler():
-    dom=UniformGridFcts((-1,1,10),(-2,3,3))
+    dom=UniformGridFcts((-1,1,10),(-2,3,5))
     F=KL(dom,w=dom.ones())
     ft.test_functional(F)
     
     ft.test_functional(HorizontalShiftDilation(F,dilation=3.,shift=F.domain.ones()))
     ft.test_functional(F-2.)
 
-    F2 = KL(dom,w=dom.ones(),quad_taylor_l=0.5,constr_u=5.)
-    ft.test_functional(F2,test_second_deriv_conj=False)
-    ft.test_functional(4.*F2+LinearFunctional(F2.domain.ones(),domain=F2.domain),test_second_deriv_conj=False)
-    ft.test_functional(HorizontalShiftDilation(F2,dilation=3.,shift=-F2.domain.ones()),test_second_deriv_conj=False)
 
-    w=1.+0.5*np.sin(dom.coords[0]*dom.coords[1])
-    F3 = KL(dom,w=5*dom.ones(),lin_taylor_l=0.1,quad_taylor_u=2.5)
+    w=1.+0.5*np.sin(dom.coords[0]*dom.coords[1])   
+    F2 = KL(dom,w=w,quad_taylor_l=0.5,quad_taylor_u=5.)
+    ft.test_functional(F2)
+    ft.test_functional(4.*F2+LinearFunctional(F2.domain.ones(),domain=F2.domain))
+    ft.test_functional(HorizontalShiftDilation(F2,dilation=3.,shift=-F2.domain.ones()))
+
+    F3 = KL(dom,w=4.*dom.ones(),lin_taylor_l=0.1,quad_taylor_u=2.5)
     ft.test_functional(F3)
 
     F4 = KL(dom,w=dom.ones(),constr_l=0.3,lin_taylor_u=2.5)
-    ft.test_functional(F4)
+    ft.test_functional(F4,test_second_deriv=False,test_second_deriv_conj=False)
 
 def test_relative_entropy():
     dom=UniformGridFcts((-5,7,4),(100,200,3))