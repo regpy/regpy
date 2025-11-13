5d796c306b2e53d15418741e8270c5850ebfbc1b
Thorsten Hohage <Hohage@math.uni-goettingen.de>
2025-11-13 16:32:09 +0100

    added vector Laplacian and more tests

--- tests/operator_tests/convolution_operator.py
+++ tests/operator_tests/convolution_operator.py
@@ -121,7 +121,9 @@ def test_differential_operators():
 
     grad =  gradient(grid.vector_valued_space(1),pad_amount=pad_amount)
     div = divergence(grid.vector_valued_space(2),pad_amount=pad_amount)  
-    Lap = Laplacian(grid.scalar_space(),pad_amount=pad_amount)
+    Lap = Laplacian(grid,pad_amount=pad_amount)
+    div_grad = div.composition(grad)
+    assert np.allclose(Lap.fourier_multiplier,div_grad.fourier_multiplier)
 
     errors += op_evaluation_and_ot(grad)
     collect_errors(gradient,errors)
@@ -130,19 +132,23 @@ def test_differential_operators():
     errors += op_evaluation_and_ot(Lap)
     collect_errors(Laplacian,errors)
 
-    # test identities curl grad = 0 and div curl =0 
+    # test identities curl grad = 0,  div curl = 0, and \Delta = grad div - curl curl
     for type in [float,complex]:
         for pad_amount in [None,  [2,0,3]]: # different implementations of convolution operators with and without padding 
             grid3D = UniformGridFcts((-pi,pi,20), (-pi,pi,24),(-pi,pi,15),dtype=type,shape_codomain=(1,))
 
             grad = gradient(grid3D.vector_valued_space(1),pad_amount=pad_amount)   
             curlop = curl(grid3D.vector_valued_space(3),pad_amount=pad_amount)
-            div = divergence(grid3D.vector_valued_space(3),pad_amount=pad_amount)    
+            div = divergence(grid3D.vector_valued_space(3),pad_amount=pad_amount)
+            Lap3D = Laplacian(grid3D.vector_valued_space(3),pad_amount=pad_amount)
 
             curl_grad =  curlop.composition(grad)
             assert norm(curl_grad.fourier_multiplier)==0
             div_curl =  div.composition(curlop)
             assert norm(div_curl.fourier_multiplier)==0
+            test = grad.composition(div) - curlop.composition(curlop) 
+            test -= Lap3D
+            assert norm(test.fourier_multiplier)<=1e-10
 
             # make sure this also holds true approximately with periodization errors
             X,Y,Z = grid3D.coords
@@ -155,3 +161,4 @@ def test_differential_operators():
 
             assert np.allclose(curlop(grad(f3d)),np.zeros_like(g3d))
             assert np.allclose(grad.adjoint(curlop(g3d)),np.zeros_like(f3d))
+            assert np.allclose(grad(div(g3d))-curlop.adjoint(curlop(g3d)) ,  Lap3D(g3d),atol=1e-6)
