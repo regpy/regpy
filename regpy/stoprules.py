b30cb0f2ca402eecc4fcb1e8e9aefdc4148cb8ab
Frank Werner <frank.werner@uni-wuerzburg.de>
2026-01-07 20:05:21 +0100

    Lepskii noch unfertig

--- regpy/stoprules.py
+++ regpy/stoprules.py
@@ -597,6 +597,69 @@ class QuasiOpt(StopRule):
 
     def best_iterate(self):
         return self.recos[self.best_stopping_index()]
+    
+class Lepskii(StopRule):
+        """Lepskii principle.
+
+        Computes x for all available parameters
+        and returns as best iterate x_{\bar k} where
+        \bar k =  max{ k=1,...,max_it | ||x_l - x_k|| <=  4 solver.error_prop(l) for all l<= k} 
+        
+
+        Parameters
+        ----------
+        setting: Setting
+        solver: The solver used for computing the reconstructions x
+        max_iter: int
+            Maximal number of regularization parameters considered
+        """
+        def __init__(self, 
+                     setting,
+                     solver,
+                     max_iter:int=1000
+                    ):
+            from regpy.solvers import Setting
+            super().__init__()
+            self.data = setting.data
+            self.norm = setting.h_codomain.norm
+            self.history_dict["error_prop"] = []
+            self.history_dict["alphas"] = []
+            self.recos=[]
+            self.max_iter = max_iter
+            self.it =0
+
+        def __repr__(self):
+            return 'Lepskii'
+
+        def _stop(self):
+            self.it +=1
+            if self.solver.y is None:
+                raise MissingValueError
+            if self.solver.x is None:
+                raise MissingValueError
+            self.history_dict["alphas"].append(self.solver.alpha)
+            self.history_dict["error_prop"].append(self.solver.error_prop)
+            self.recos.append(self.solver.x.copy())
+            self.log_info = 'alpha {:.3e}'.format(self.solver.alpha)
+            return self.it >= self.max_iter
+        
+        def best_stopping_index(self):
+            k = 1
+            error_prop = self.history_dict["error_prop"]
+            norm_diff = 0
+            self.history_dict["max_change"] = []
+            while np.all(norm_diff <= 4*error_prop[:k-1]) & k<self.max_iter-1:
+                norm_diff = []
+                for x in self.recos[:k-1]:
+                    norm_diff.append(self.norm(self.recos[k] - x))
+                k +=1
+                print(norm_diff)
+                self.history_dict["max_change"].append(np.max(norm_diff))
+            return k-1 
+        
+        def best_iterate(self):
+            return self.recos[self.best_stopping_index()]
+
 
 class Oracle(StopRule):
     """Oracle stopping rule. Returns the iterate that is closest to the exact solution in terms of the given distance function.
